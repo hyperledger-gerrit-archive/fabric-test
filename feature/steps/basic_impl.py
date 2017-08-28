a0d6bb15a5a461d38d10e66583d7d2b9f5329750
Adnan Choudhury <adnan.choudhury@itpeoplecorp.com>
2017-08-28 18:04:41 -0400

    WIP [FAB-4667] Gossip test for leader peer
    Gossip leader stops/pauses/disconnects and the non-leader peer becomes
    leader, and the initial leader comes back up, is supposed to get all
    blocks generated when it was done.
    
    (WIP)
    
    Change-Id: I749ab512cf072d53e3a9984c82ebd3abb2ba62a1
    Signed-off-by: Adnan Choudhury <adnan.choudhury@itpeoplecorp.com>

--- feature/steps/basic_impl.py
+++ feature/steps/basic_impl.py
@@ -134,3 +134,11 @@ def start_network_impl(context, ordererType):
 @when(u'I start a fabric network')
 def step_impl(context):
     start_network_impl(context, "solo")
+
+@given(u'the initial non-leader peer of "{org}" has become the leader')
+def step_impl(context, org):
+    if not hasattr(context, 'initial_non_leader'):
+        assert False, "Error: initial non-leader was not set previously. This statement works only with pre-set initial non-leader."
+    else:
+        if not endorser_util.is_in_log(context.initial_non_leader[org], "Becoming a leader"):
+            assert False, "Error: initial non-lerader peer has not become leader."
