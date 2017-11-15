286f2a60a8ab4e4ce732ae0a2643583941c4c579
Surya <suryalnvs@gmail.com>
2017-11-15 16:39:05 -0500

    [FAB-6997] Automation scripts for OTE testcases
    Automation scripts for running orderer performance tests (v1.1)
    using OTE. Submitting a basic testcase (FAB-6996) to OTE and
    fixing README.md and ote.go.
    
    Change-Id: Idea548396906893b4667a5a41902cb58b7f16af4
    Signed-off-by: Surya <suryalnvs@gmail.com>

--- regression/daily/runDailyTestSuite.sh
+++ regression/daily/runDailyTestSuite.sh
@@ -27,3 +27,6 @@ py.test -v --junitxml results_ledger_lte.xml ledger_lte.py
 
 echo "========== Fabric-CA ACL smoke test..."
 py.test -v --junitxml results_acl.xml acl_happy_path.py
+
+echo "========== Orderer Performance tests..."
+py.test -v --junitxml results_orderer_ote.xml orderer_ote.py
