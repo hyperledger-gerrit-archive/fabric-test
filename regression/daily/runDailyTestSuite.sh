3a110e9e6744500fea641935bd68e374b31e1205
Surya <suryalnvs@gmail.com>
2017-11-15 16:39:05 -0500

    [FAB-6997] Automation scripts for OTE testcases
    Automation scripts for running orderer performance tests (v1.1)
    using OTE. Submitting a basic testcase (FAB-6996) to OTE and
    fixing README.md and ote.go.
    
    Change-Id: Idea548396906893b4667a5a41902cb58b7f16af4
    Signed-off-by: Surya <suryalnvs@gmail.com>

--- regression/daily/runDailyTestSuite.sh
+++ regression/daily/runDailyTestSuite.sh
@@ -7,23 +7,26 @@
 
 DAILYDIR="$GOPATH/src/github.com/hyperledger/fabric-test/regression/daily"
 
-#echo "========== Sample Tests..."
-#py.test -v --junitxml results_sample.xml Example.py
+##echo "========== Sample Tests..."
+##py.test -v --junitxml results_sample.xml Example.py
 
-echo "========== System Test Performance Stress tests driven by PTE tool..."
-py.test -v --junitxml results_systest_pte.xml systest_pte.py
+#echo "========== System Test Performance Stress tests driven by PTE tool..."
+#py.test -v --junitxml results_systest_pte.xml systest_pte.py
 
-echo "========== Behave feature and system tests..."
-cd ../../feature
-behave --junit --junit-directory ../regression/daily/. --tags=-skip --tags=daily -k -D logs=y
-cd -
+#echo "========== Behave feature and system tests..."
+#cd ../../feature
+#behave --junit --junit-directory ../regression/daily/. --tags=-skip --tags=daily -k -D logs=y
+#cd -
 
-echo "========== Ledger component performance tests..."
-py.test -v --junitxml results_ledger_lte.xml ledger_lte.py
+#echo "========== Ledger component performance tests..."
+#py.test -v --junitxml results_ledger_lte.xml ledger_lte.py
 
-# the auction_daily test is disabled until the CI issue is fixed
-#echo "========== Test Auction Chaincode ..."
-#py.test -v --junitxml results_auction_daily.xml testAuctionChaincode.py
+## the auction_daily test is disabled until the CI issue is fixed
+##echo "========== Test Auction Chaincode ..."
+##py.test -v --junitxml results_auction_daily.xml testAuctionChaincode.py
 
-echo "========== Fabric-CA ACL smoke test..."
-py.test -v --junitxml results_acl.xml acl_happy_path.py
+#echo "========== Fabric-CA ACL smoke test..."
+#py.test -v --junitxml results_acl.xml acl_happy_path.py
+
+echo "========== Orderer Performance tests..."
+py.test -v --junitxml results_orderer_ote.xml orderer_ote.py
