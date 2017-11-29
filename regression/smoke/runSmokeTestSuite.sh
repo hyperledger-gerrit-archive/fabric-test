ebcf7714ca7e1f7fb6e0203ec4ff21088bb8146b
Adnan Choudhury <adnan.choudhury@itpeoplecorp.com>
2017-11-29 15:19:46 -0500

    test FAB-6333
    Change-Id: I5687cb7c90ec8b695d6c480ac478b1241cf0df8f
    Signed-off-by: Adnan Choudhury <adnan.choudhury@itpeoplecorp.com>

--- regression/smoke/runSmokeTestSuite.sh
+++ regression/smoke/runSmokeTestSuite.sh
@@ -8,9 +8,6 @@ SMOKEDIR="$GOPATH/src/github.com/hyperledger/fabric-test/regression/smoke"
 
 echo "========== Behave feature and system tests..."
 cd ../../feature
-behave --junit --junit-directory ../regression/smoke/. --tags=-skip --tags=smoke -k -D logs=y
+behave --junit --junit-directory ../regression/smoke/. --tags=testFAB6333 -k -D logs=y
 cd -
-
-echo "========== System Test Performance Stress tests driven by PTE tool..."
-py.test -v --junitxml results_systest_pte.xml systest_pte.py
 
