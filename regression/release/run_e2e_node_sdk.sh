848b1e7bcb965c1511ffddfd9788edf8013e4379
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-02-24 12:42:06 -0500

    FAB-8501 updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Change-Id: I78b71a2516c1d7f62b232f0ade76eaf0e0aa592a
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- regression/release/run_e2e_node_sdk.sh
+++ regression/release/run_e2e_node_sdk.sh
@@ -12,14 +12,9 @@ WD="$GOPATH/src/github.com/hyperledger/fabric-sdk-node"
 SDK_REPO_NAME=fabric-sdk-node
 git clone https://github.com/hyperledger/$SDK_REPO_NAME $WD
 cd $WD
-git checkout tags/v1.0.0
-cd test/fixtures
-docker rm -f "$(docker ps -aq)" || true
-docker-compose up >> node_dockerlogfile.log 2>&1 &
-sleep 10
-docker ps -a
-cd ../.. && npm install
-npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
+# checkout to latest release tag
+git checkout tags/v1.1.0-alpha
+npm install && npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
 gulp || true
 gulp ca || true
 rm -rf node_modules/fabric-ca-client && npm install
