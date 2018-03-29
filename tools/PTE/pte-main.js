11dcea2ef3954ca9372870615bd34b153921af5e
Dongming <dming@us.ibm.com>
2018-03-29 13:25:31 -0400

    [FAB-9210] PTE defines path in input json
    channelTX: relateive path to gopath/src if gopath
    is defined, else otherwise absolute path
    chaincodePath: reltive path to gopath/src if golang,
    and absolute path for node chaincode
    metadataPath: reltive path to gopath/src if gopath
    is defined, otherwise absolute path
    
    Change-Id: I6ad138d5095e15067b625a4fcc8cf4e8d82fa145
    Signed-off-by: Dongming <dming@us.ibm.com>

--- tools/PTE/pte-main.js
+++ tools/PTE/pte-main.js
@@ -126,7 +126,11 @@ function initDeploy() {
     logger.info('chaincode language: %s, path: %s', language, chaincodePath);
 
     if ((typeof( uiContent.deploy.metadataPath ) !== 'undefined')) {
-        metadataPath=uiContent.deploy.metadataPath;
+        if (typeof(goPath) !== 'undefined') {
+            metadataPath = path.join(goPath, 'src', uiContent.deploy.metadataPath);
+        } else {
+            metadataPath=uiContent.deploy.metadataPath;
+        }
         logger.info('metadataPath: %s', metadataPath);
     }
 }
@@ -681,6 +685,9 @@ function createOneChannel(client ,channelOrgName) {
     hfc.setConfigSetting('key-value-store', 'fabric-client/lib/impl/FileKeyValueStore.js');
 
     var channelTX=channelOpt.channelTX;
+    if ( typeof(goPath) !== 'undefined' ) {
+        channelTX = path.join(goPath, 'src', channelOpt.channelTX);
+    }
     logger.info('[createOneChannel] channelTX: ', channelTX);
     envelope_bytes = fs.readFileSync(channelTX);
     config = client.extractChannelConfig(envelope_bytes);
