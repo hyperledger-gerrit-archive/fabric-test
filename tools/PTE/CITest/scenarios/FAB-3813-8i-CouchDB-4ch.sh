ea73d940565d17e92a1c79ce68e01577aaf94de3
Nao Nishijima <nao.nishijima@hal.hitachi.com>
2018-05-01 11:37:41 -0700

    [FAB-3813] TPS invokes 8threads CouchDB 4channels
    Change-Id: I37569298ae8bd194186e1a4c85f70cd63b6fbaf1
    Signed-off-by: Nao Nishijima <nao.nishijima@hal.hitachi.com>

---
+++ tools/PTE/CITest/scenarios/FAB-3813-8i-CouchDB-4ch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Copyright Hitachi America. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+########## CI test ##########
+
+CWD=$PWD
+PREFIX="result"   # result log prefix
+
+cd ../scripts
+
+#### Launch network and execute testcase FAB-3813-8i-CouchDB-4ch: 4 threads invokes, CouchDB
+./test_driver.sh -n -m FAB-3813-8i-CouchDB-4ch -p -c samplecc -t FAB-3813-8i-CouchDB-4ch
+#### gather TPS from docker containers
+./get_peerStats.sh -r FAB-3813-8i-CouchDB-4ch -p peer0.org1.example.com peer0.org2.example.com -c testorgschannel1 testorgschannel2 testorgschannel3 testorgschannel4 -n $PREFIX -o $CWD -v
