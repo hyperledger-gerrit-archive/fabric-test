a2953439071c8838352eb1fdaf50337b57a04b48
Scott Zwierzynski <scottz@us.ibm.com>
2018-05-08 19:54:03 -0400

    [FAB-9960] fix daily PTE test assertion errs =WIP=
    + Tweak get_peerStats.sh to modify the output in
    the PTE summary report, so we can easily grep
    only the summary of the invokes from each peer.
    + Fix the assertion statements, and comments,
    accordingly in the systest_pte script.
    + WIP: run the pte tests from the verify job
    
    Change-Id: I3c7009a99b1751870bb3b801aabce17a3f7e3e0d
    Signed-off-by: Scott Zwierzynski <scottz@us.ibm.com>

--- regression/smoke/runSmokeTestSuite.sh
+++ regression/smoke/runSmokeTestSuite.sh
@@ -6,14 +6,19 @@
 
 SMOKEDIR="$GOPATH/src/github.com/hyperledger/fabric-test/regression/smoke"
 
-echo "========== Behave feature and system tests..."
-cd ../../feature
-behave --junit --junit-directory ../regression/smoke/. --tags=-skip --tags=smoke -k -D logs=y
-cd -
+###   echo "========== Behave feature and system tests..."
+###   cd ../../feature
+###   behave --junit --junit-directory ../regression/smoke/. --tags=-skip --tags=smoke -k -D logs=y
+###   cd -
 
 echo "========== System Test using PTE and NL tools..."
-py.test -v --junitxml results_systest_pte.xml systest_pte.py
+echo "Execute the PTE tests in the daily folder that I am fixing..."
+cp -r ../../tools/PTE $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/
+cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node
+./../pre_setup.sh && npm config set prefix ~/npm && npm install && npm install -g gulp
+gulp ca && cd $SMOKEDIR/../daily && py.test -v --junitxml results_systest_pte.xml systest_pte.py
+cd $SMOKEDIR
 
-echo "========== Orderer component test using OTE and NL tools..."
-py.test -v --junitxml results_orderer_ote.xml orderer_ote.py
+###   echo "========== Orderer component test using OTE and NL tools..."
+###   py.test -v --junitxml results_orderer_ote.xml orderer_ote.py
 
