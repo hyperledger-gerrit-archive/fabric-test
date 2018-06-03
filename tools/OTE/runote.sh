8a4652ae22a09a82d66249bc8c997783daabd8dd
Scott Zwierzynski <scottz@us.ibm.com>
2018-06-03 18:58:37 -0400

    [FAB-10515] OTE: increase delivery timeout
    To accomodate possible delays in block deliveries
    on a single host machine, we increase the
    slowRecoveryTimeout. And add related comments.
    And output the top 20 lines of the CPU usage data
    for every test, not just the failing tests.
    
    Change-Id: Ie8548ca26d658c0fa796ea77c55af6e5df360795
    Signed-off-by: Scott Zwierzynski <scottz@us.ibm.com>

--- tools/OTE/runote.sh
+++ tools/OTE/runote.sh
@@ -347,13 +347,13 @@ echo "====== $ df"
 df
 echo "====== $ free"
 free
+echo "====== $ top"
+top -b -n 1 | head -n 20
 
 # Check the output OTE test logs for the string "RESULT=PASSED' which ote.go prints for each
 # successfully passed testcase. If an error occurred, collect container logs and host data.
 if [ `grep -c RESULT=PASSED ./logs/${TESTCASE}.log` -eq 0 ]
 then
-    echo "====== $ top"
-    top -b -n 1
     echo "====== Saving all docker container logs in logs/ for the ${TESTCASE} test failure."
     saveOrdLogs
     saveLogs "kafka" $KBS
