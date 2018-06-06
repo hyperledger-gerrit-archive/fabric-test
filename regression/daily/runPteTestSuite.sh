eff84674b2b81b283288f41ff347a94aaad0556a
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-06-06 14:14:32 -0400

    FAB-10567 Create new tests and make targets.
    Create new tests and the make targets to
    pull images from Nexus instead of building
    them and run the daily, smoke and PTE tests.
    
    These tests can be run from the local host
    or utilised in the CI tests.
    
    Change-Id: I781d9aa6704968c4de693dc3f02ea59719bc92b1
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

---
+++ regression/daily/runPteTestSuite.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+DAILYDIR="$GOPATH/src/github.com/hyperledger/fabric-test/regression/daily"
+cd $DAILYDIR
+
+echo "========== System Test Performance tests using PTE and NL tools..."
+cd $GOPATH/src/github.com/hyperledger/fabric-test/tools/PTE
+
+npm install
+if [ $? != 0 ]; then
+   echo "------> Could not install npm, exiting test.." && exit 1
+else
+   echo "------> npm is installed"
+fi
+
+cd $DAILYDIR && py.test -v --junitxml results_systest_pte.xml systest_pte.py
