1cb3bb06e33619112d6a56932d9e5eab9cea163c
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-06-06 14:14:32 -0400

    FAB-10567 Create new tests and make targets.
    Create new tests and the make targets to
    pull images from Nexus instead of building
    them and run the daily, smoke and PTE tests.
    
    These tests can be run from the local host
    or utilised in the CI tests.
    
    Change-Id: I781d9aa6704968c4de693dc3f02ea59719bc92b1
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>
    Signed-off-by: Ramesh Babu Thoomu <ramesh.thoomu@chainyard.com>

--- regression/smoke/runSmokeTestSuite.sh
+++ regression/smoke/runSmokeTestSuite.sh
@@ -7,18 +7,24 @@
 SMOKEDIR="$GOPATH/src/github.com/hyperledger/fabric-test/regression/smoke"
 cd $SMOKEDIR
 
-echo "========== Behave feature and system tests..."
+echo "-----------> Behave feature and system tests..."
 cd ../../feature
 behave --junit --junit-directory ../regression/smoke/. --tags=-skip --tags=smoke -k -D logs=y
 cd -
 
-echo "========== System Test using PTE and NL tools..."
-cp -r ../../tools/PTE $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node/test/
-cd $GOPATH/src/github.com/hyperledger/fabric-test/fabric-sdk-node
-./../pre_setup.sh && npm config set prefix ~/npm && npm install && npm install -g gulp
-gulp ca && cd $SMOKEDIR && py.test -v --junitxml results_systest_pte.xml systest_pte.py
-cd $SMOKEDIR
+echo "------------> Performance Test using PTE and NL tools..."
+./../../scripts/pre_setup.sh
+cd $GOPATH/src/github.com/hyperledger/fabric-test/tools/PTE
+npm config set prefix ~/npm
+npm install
+  if [ $? != 0 ]; then
+     echo "------------> Failed to install npm. Cannot run pte test suite."
+     exit 1
+  else
+     echo "------------> Successfully installed npm."
+  fi
 
-echo "========== Orderer component test using OTE and NL tools..."
+cd $SMOKEDIR && py.test -v --junitxml results_systest_pte.xml systest_pte.py
+
+echo "------------> Orderer component test using OTE and NL tools..."
 py.test -v --junitxml results_orderer_ote.xml orderer_ote.py
-
