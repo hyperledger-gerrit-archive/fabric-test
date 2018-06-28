470f5aa3148ec919c0b055a65575c2932790edef
Latitia M Haskins <latitia.haskins@gmail.com>
2018-06-28 17:48:05 -0400

    [FAB-10920] Add certificates before setting objects
    The orderer and channel objects were being set before
    setting the crypto suite for the requests. This
    rearranges the order of operations to be sure the
    crypto suite is set first.
    
    Change-Id: I4438267dcd9178675d7fa7ec453295731791cd77
    Signed-off-by: Latitia M Haskins <latitia.haskins@gmail.com>

--- feature/sdk/node/invoke.js
+++ feature/sdk/node/invoke.js
@@ -33,7 +33,6 @@ var invoke = function(username, org, chaincode, peerNames, orderer, network_conf
     }
 
     let channel = client.newChannel(chaincode.channelId);
-    channel.addOrderer(common.newOrderer(client, network_config['network-config'], orderer, network_config['tls']));
 
     common.setupPeers(peerNames, channel, org, client, network_config['network-config'], network_config['tls']);
     let targets = (peerNames) ? common.newPeers(peerNames, org, network_config['network-config'], client) : undefined;
@@ -41,6 +40,9 @@ var invoke = function(username, org, chaincode, peerNames, orderer, network_conf
     let tx_id = null;
     return common.getRegisteredUsers(client, username, username.split('@')[1], network_config['networkID'], network_config['network-config'][org]['mspid']).then((user) => {
         tx_id = client.newTransactionID();
+
+        channel.addOrderer(common.newOrderer(client, network_config['network-config'], orderer, network_config['tls']));
+
         // send proposal to endorser
         let request = {
             chaincodeId: chaincode.chaincodeId,
