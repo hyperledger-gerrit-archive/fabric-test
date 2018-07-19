fdf68be7184c91ba9231f8bdeb3a2788f84eccbc
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-07-18 22:27:47 -0400

    [FAB-11186] WIP: Added the 12HrTest PTE test.
    Change-Id: I8ee551214fde9b72348d0ffbafbd852fc556ca07
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

---
+++ regression/weekly/run12HrTest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+######################################################################
+### Run one group of the tests in weekly test suite.
+
+echo "========== Performance PTE 12Hr test"
+py.test -v --junitxml results_TimedRun_12Hr.xml 12HrTest.py -k TimedRun_12Hr
