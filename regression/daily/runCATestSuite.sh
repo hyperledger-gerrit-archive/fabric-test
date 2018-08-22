f1eb05c2a0a62ecbcae5c5d57b5378158ba9b5f6
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-08-21 20:14:54 -0400

    FABCI-9 Archive the daily test artifacts to Nexus.
    The daily test artifacts for behave,PTE,LTE,OTE and
    CA are retained just for 7 days on Jenkins. This patch
    will archive the artifacts in Nexus for a long duration.
    
    Change-Id: I9b7b65ed2cae1da88f6cc2b331bb59a442081d89
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

--- regression/daily/runCATestSuite.sh
+++ regression/daily/runCATestSuite.sh
@@ -7,8 +7,18 @@
 DAILYDIR="$GOPATH/src/github.com/hyperledger/fabric-test/regression/daily"
 cd $DAILYDIR
 
+archiveCA() {  
+    echo "-----> Archiving generated logs"
+    rm -rf $WORKSPACE/archives
+    mkdir -p $WORKSPACE/archives/CA_Test_Logs
+    cp -r $GOPATH/src/github.com/hyperledger/fabric-test/fabric-samples/fabric-ca/data/logs/*.log $WORKSPACE/archives/CA_Test_Logs/
+    mkdir -p $WORKSPACE/archives/CA_Test_XML
+    cp -r $GOPATH/src/github.com/hyperledger/fabric-test/regression/daily/*.xml $WORKSPACE/archives/CA_Test_XML/
+}
+
 echo "======== Fabric-CA ACL smoke test... ========"
-py.test -v --junitxml results_acl.xml acl_happy_path.py
+py.test -v --junitxml results_acl.xml acl_happy_path.py && echo "------> Fabric-CA ACL smoke-test completed."
 
 echo "======== Fabric-CA tests...========"
-py.test -v --junitxml results_fabric-ca_tests.xml ca_tests.py
+py.test -v --junitxml results_fabric-ca_tests.xml ca_tests.py && echo "------> Fabric-CA tests completed."
+archiveCA
--- regression/daily/runOteTestSuite.sh
+++ regression/daily/runOteTestSuite.sh
@@ -7,5 +7,14 @@
 DAILYDIR="$GOPATH/src/github.com/hyperledger/fabric-test/regression/daily"
 cd $DAILYDIR
 
+archiveOTE() {  
+    echo "-----> Archiving generated logs"
+    rm -rf $WORKSPACE/archives
+    mkdir -p $WORKSPACE/archives/OTE_Test_Logs
+    cp -r $GOPATH/src/github.com/hyperledger/fabric-test/regression/daily/ote_logs/*.log $WORKSPACE/archives/OTE_Test_Logs/
+    mkdir -p $WORKSPACE/archives/OTE_Test_XML
+    cp -r $GOPATH/src/github.com/hyperledger/fabric-test/regression/daily/*.xml $WORKSPACE/archives/OTE_Test_XML/
+} 
 echo "======== Orderer Performance tests...========"
-py.test -v --junitxml results_orderer_ote.xml orderer_ote.py
+py.test -v --junitxml results_orderer_ote.xml orderer_ote.py && echo "-----> OTE Tests completed"
+archiveOTE
