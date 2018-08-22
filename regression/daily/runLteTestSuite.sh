6eb5faeb2082b3ceae9a2f927e5cb28206bc20f6
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-08-21 20:14:54 -0400

    FABCI-9 Archive the daily test artifacts to Nexus.
    The daily test artifacts for behave,PTE,LTE,OTE and
    CA are retained just for 7 days on Jenkins. This patch
    will archive the artifacts in Nexus for a long duration.
    
    Change-Id: I9b7b65ed2cae1da88f6cc2b331bb59a442081d89
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

--- regression/daily/runLteTestSuite.sh
+++ regression/daily/runLteTestSuite.sh
@@ -7,5 +7,18 @@
 DAILYDIR="$GOPATH/src/github.com/hyperledger/fabric-test/regression/daily"
 cd $DAILYDIR
 
+archiveLTE() {
+if [ ! -z "$(echo $GERRIT_BRANCH)" ]; then
+# WORKSPACE is a Jenkins parameter.This condition is used only when the test is run in Jenkins to archive the log files.
+    echo "------> Archiving generated logs"
+    rm -rf $WORKSPACE/archives
+    mkdir -p $WORKSPACE/archives/LTE_Test_Logs
+    cp -r $GOPATH/src/github.com/hyperledger/fabric-test/regression/daily/*.log $WORKSPACE/archives/LTE_Test_Logs/
+    mkdir -p $WORKSPACE/archives/LTE_Test_XML
+    cp -r $GOPATH/src/github.com/hyperledger/fabric-test/regression/daily/*.xml $WORKSPACE/archives/LTE_Test_XML/
+fi
+}
+
 echo "======== Ledger component performance tests...========"
-py.test -v --junitxml results_ledger_lte.xml ledger_lte.py
+py.test -v --junitxml results_ledger_lte.xml ledger_lte.py && echo "------> LTE Tests completed."
+archiveLTE
