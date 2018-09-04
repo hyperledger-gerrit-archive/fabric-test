9ea5857c2f1dd31f902152d119ed1aedfefa278d
Dongming <dming@us.ibm.com>
2018-09-04 14:00:49 -0400

    [FAB-10678] PTE testcases use get_pteReport.js
    This CR is to update all PTE testcases to use
    get_pteReport.js to get overall TPS
    
    Change-Id: Ib38a3df1e23f15ac65b45695a9bb5713be7b7054
    Signed-off-by: Dongming <dming@us.ibm.com>

--- tools/PTE/CITest/scenarios/FAB-3833-2i.sh
+++ tools/PTE/CITest/scenarios/FAB-3833-2i.sh
@@ -9,22 +9,42 @@
 ########## CI test ##########
 
 CWD=$PWD
-PREFIX="result"   # result log prefix
+TESTCASE='FAB-3833-2i'
+pteReport='../../pteReport.txt'
+CIpteReport='../Logs/'$TESTCASE'-pteReport.txt'
 
 cd ../scripts
 
+# remove existing pteReport
+if [ -e $pteReport ]; then
+    echo "remove $pteReport"
+    rm -f $pteReport
+fi
+
 
 #### Launch network and synch-up ledger
-./test_driver.sh -n -m FAB-3833-2i -p -c samplecc -t FAB-3810-2q
+./test_driver.sh -n -m $TESTCASE -p -c samplecc -t FAB-3810-2q
 #### remove PTE log from synch-up ledger run
 rm -f ../Logs/FAB-3810-2q*.log
+# remove pteReport from priming query
+if [ -e $pteReport ]; then
+    echo "remove $pteReport"
+    rm -f $pteReport
+fi
+
+
 #### execute testcase FAB-3833-2i: 2 threads invokes, couchDB
-./test_driver.sh -t FAB-3833-2i
-#### gather TPS from docker containers
-./get_peerStats.sh -r FAB-3833-2i -p peer0.org1.example.com peer0.org2.example.com -c testorgschannel1 -n $PREFIX -o $CWD -v
+./test_driver.sh -t $TESTCASE
+#### calculate overall invoke TPS from pteReport
+node get_pteReport.js $pteReport
+mv $pteReport $CIpteReport
+
 
 #### execute testcase FAB-3810-2q: 2 threads queries, couchDB
 ./test_driver.sh -t FAB-3810-2q
-#### gather TPS from PTE log
-grep Summary ../Logs/FAB-3810-2q*.log | grep "QUERY" >> $CWD/$PREFIX"_FAB-3833-2i.log"
+#### calculate overall query TPS from pteReport
+node get_pteReport.js $pteReport
+cat $pteReport >> $CIpteReport
+grep Summary ../Logs/FAB-3810-2q*.log | grep "QUERY" >> $CIpteReport
 
+echo "$TESTCASE test completed."
