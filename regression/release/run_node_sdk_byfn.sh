ee47ab6e4629becb24313dbbfa3d12c6bffa7aff
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

--- regression/release/run_node_sdk_byfn.sh
+++ regression/release/run_node_sdk_byfn.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -eu
-
-#
-# Copyright IBM Corp. All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
 #
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 
 # RUN END-to-END Test
 #####################
@@ -12,22 +16,29 @@ rm -rf ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node
 
 WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-node"
 SDK_REPO_NAME=fabric-sdk-node
-git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$SDK_REPO_NAME $WD
+git clone git://cloud.hyperledger.org/mirror/$SDK_REPO_NAME $WD
 cd $WD
-git checkout tags/v1.0.0
+git checkout $GERRIT_BRANCH
+git checkout $FABRIC_SDK_NOD_REL_COMMIT
+
+#git checkout $RELEASE_COMMIT
 NODE_SDK_COMMIT=$(git log -1 --pretty=format:"%h")
 
-echo "FABRIC NODE SDK COMMIT ========> " $NODE_SDK_COMMIT >> ${WORKSPACE}/gopath/src/github.com/hyperledger/commit_history.log
+echo "FABRIC NODE SDK COMMIT ========> " $NODE_SDK_COMMIT >> \
+${WORKSPACE}/gopath/src/github.com/hyperledger/commit_history.log
 
 cd test/fixtures
 cat docker-compose.yaml > docker-compose.log
 docker-compose up >> dockerlogfile.log 2>&1 &
 sleep 10
 docker ps -a
-cd ../.. && npm install
-npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
-gulp && gulp ca
-rm -rf node_modules/fabric-ca-client && npm install
+cd ../..
+npm install
+npm config set prefix ~/npm
+npm install -g gulp
+npm install -g istanbul
+gulp
+gulp ca
+rm -rf node_modules/fabric-ca-client
+npm install
 node test/integration/e2e.js
-
-
