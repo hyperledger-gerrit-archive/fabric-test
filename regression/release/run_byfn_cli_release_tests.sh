5841651936024c6dfae29163dab1725bdec021e5
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

--- regression/release/run_byfn_cli_release_tests.sh
+++ regression/release/run_byfn_cli_release_tests.sh
@@ -1,41 +1,102 @@
-#!/bin/bash
-#
-# Copyright IBM Corp. All Rights Reserved.
+#!/bin/bash -ue
 #
 # SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
 #
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 
 # RUN BYFN Test
 #####################
-
-CH_NAME="$1"
-rm -rf ${GOPATH}/src/github.com/hyperledger/fabric-samples
-
 WD="${GOPATH}/src/github.com/hyperledger/fabric-samples"
 REPO_NAME=fabric-samples
+export STABLE_VERSION=${STABLE_VERSION:-1.3.0-stable}
+export BASE_VERSION=${BASE_VERSION:-1.3.0}
 
-git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
-cd $WD
+clean_directory() {
 
-curl -L https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap-1.0.0.sh -o bootstrap-1.0.0.sh
-chmod +x bootstrap-1.0.0.sh
-./bootstrap-1.0.0.sh
+  rm -rf ${GOPATH}/src/github.com/hyperledger/fabric-samples
+}
 
-cd $WD/first-network
-export PATH=$WD/bin:$PATH
-echo y | ./byfn.sh -m down
+clone_repo() {
 
-     if [ -z "${CH_NAME}" ]; then
-          echo "Generating artifacts for default channel"
-          echo y | ./byfn.sh -m generate
-	  echo "setting to default channel 'mychannel'"
-          echo y | ./byfn.sh -m up -t 10
-          echo
-      else
-          echo "Generate artifacts for custom Channel"
-          echo y | ./byfn.sh -m generate -c $CH_NAME
-          echo "Setting to non-default channel $CH_NAME"
-          echo y | ./byfn.sh -m up -c $CH_NAME -t 10
-          echo
-     fi
-echo y | ./byfn.sh -m down
+  git clone git://cloud.hyperledger.org/mirror/$REPO_NAME $WD
+  #git checkout $RELEASE_COMMIT
+
+  # Display the RELEASE_VERSION to indicate the RELEASE being tested.
+  echo "--------> RELEASE_VERSION : $RELEASE_VERSION"
+}
+
+# pull fabric binaries
+pull_fabric_binaries() {
+  export MARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
+  echo "-------> MARCH:" $MARCH
+  echo "-------> pull stable binaries for all platforms (x and z)"
+  MVN_METADATA=$(echo "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric-$STABLE_VERSION/maven-metadata.xml")
+  curl -L "$MVN_METADATA" > maven-metadata.xml
+  RELEASE_TAG=$(cat maven-metadata.xml | grep release)
+  COMMIT=$(echo $RELEASE_TAG | awk -F - '{ print $4 }' | cut -d "<" -f1)
+  echo "-------> COMMIT:" $COMMIT
+  curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric-$STABLE_VERSION/$MARCH.$STABLE_VERSION-$COMMIT/hyperledger-fabric-$STABLE_VERSION-$MARCH.$STABLE_VERSION-$COMMIT.tar.gz | tar xz
+  if [ $? != 0 ]; then
+    echo "-------> FAILED to pull fabric binaries"
+    exit 1
+  fi
+
+  cp -r $WORKSPACE/gopath/src/github.com/hyperledger/fabric/release/linux-amd64/bin/ .
+}
+
+run_tests() {
+
+  echo "############## BYFN,EYFN DEFAULT CHANNEL TEST ###########"
+  echo "#########################################################"
+
+  echo y | ./byfn.sh -m down
+  echo y | ./byfn.sh -m up -t 60
+  copy_logs $? default-channel
+  echo y | ./eyfn.sh -m up -t 60
+  copy_logs $? default-channel
+  echo y | ./eyfn.sh -m down
+  echo
+  echo "############## BYFN,EYFN CUSTOM CHANNEL TEST ############"
+  echo "#########################################################"
+
+  echo y | ./byfn.sh -m up -c fabricrelease -t 60
+  copy_logs $? fabricrelease
+  echo y | ./eyfn.sh -m up -c fabricrelease -t 60
+  copy_logs $? fabricrelease
+  echo y | ./eyfn.sh -m down
+  echo
+  echo "############# BYFN,EYFN CUSTOM CHANNEL WITH COUCHDB TEST ##############"
+  echo "#######################################################################"
+
+  echo y | ./byfn.sh -m up -c fabricrelease-couchdb -s couchdb -t 80 -d 15
+  copy_logs $? fabricrelease-couch couchdb
+  echo y | ./eyfn.sh -m up -c fabricrelease-couchdb -s couchdb -t 80 -d 15
+  copy_logs $? fabricrelease-couch couchdb
+  echo y | ./eyfn.sh -m down
+  echo
+  echo "############### BYFN,EYFN WITH NODE Chaincode. TEST ################"
+  echo "####################################################################"
+
+  echo y | ./byfn.sh -m up -l node -t 60
+  copy_logs $? default-channel-node
+  echo y | ./eyfn.sh -m up -l node -t 60
+  copy_logs $? default-channel-node
+  echo y | ./eyfn.sh -m down
+
+}
+
+main() {
+  clean_directory
+  clone_repo
+  cd $WD/first-network
+  pull_fabric_binaries
+  run_tests
+}
+
+main
