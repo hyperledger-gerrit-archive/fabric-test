443467b67ebc66a9e5165c2fcc1f7c4df8621d64
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

---
+++ regression/release/run_fabcar_release_tests.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -ue
+#
+# SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# RUN BYFN Test
+###############
+WD="${WORKSPACE}/src/github.com/hyperledger/fabric-samples"
+
+clean_directory() {
+  rm -rf $WD
+}
+
+clone_repo() {
+
+  git clone --single-branch -b $GERRIT_BRANCH \
+    git://cloud.hyperledger.org/mirror/fabric-samples $WD
+
+  (
+  cd $WD
+  git checkout $FAB_SAMPLES_REL_COMMIT
+  )
+
+}
+
+run_tests() {
+
+  (
+  cd $WD/fabcar
+
+  echo "############## FABCAR TEST ###########"
+  echo "######################################"
+  ./startFabric.sh
+  )
+
+  (
+  cd $WD/fabcar/javascript
+
+  npm install
+  node enrollAdmin
+  node registerUser
+  node invoke
+  node query
+  )
+}
+
+main() {
+  clean_directory
+  clone_repo
+  # Copy the binaries from fabric-test
+  cp -r ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-test/regression/release/fabric-samples/bin/ .
+  run_tests
+}
+
+main
