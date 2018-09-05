b0ee437db862934fb8b43eafe249a95fb6c7c61a
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

--- regression/release/run_e2e_node_sdk.sh
+++ regression/release/run_e2e_node_sdk.sh
@@ -1,25 +1,26 @@
-#!/bin/bash
+#!/bin/bash -e
 
 #
 # Copyright IBM Corp. All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
-#     
-
+#
+#
 rm -rf $GOPATH/src/github.com/hyperledger/fabric-sdk-node
-
 WD="$GOPATH/src/github.com/hyperledger/fabric-sdk-node"
 SDK_REPO_NAME=fabric-sdk-node
 git clone https://github.com/hyperledger/$SDK_REPO_NAME $WD
 cd $WD
-git checkout tags/v1.0.0
-cd test/fixtures
-docker rm -f "$(docker ps -aq)" || true
-docker-compose up >> node_dockerlogfile.log 2>&1 &
-sleep 10
-docker ps -a
-cd ../.. && npm install
-npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
+
+#curl -L https://raw.githubusercontent.com/hyperledger/fabric/${GERRIT_BRANCH}/Makefile > Makefile
+#RELEASE_VERSION=$(cat Makefile | grep "BASE_VERSION =" | awk '{print $3}')
+echo "--------> RELEASE_VERSION : $RELEASE_VERSION"
+git checkout tags/v$RELEASE_VERSION
+
+#Delete temporary Makefile.
+rm -rf Makefile
+
+npm install && npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
 gulp || true
 gulp ca || true
 rm -rf node_modules/fabric-ca-client && npm install
