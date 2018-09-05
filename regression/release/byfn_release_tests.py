10467eec896a5c764c05cc275436219fd4936519
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

--- regression/release/byfn_release_tests.py
+++ regression/release/byfn_release_tests.py
@@ -1,8 +1,13 @@
-# Copyright IBM Corp. All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
 #
-
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 import unittest
 import subprocess
 
@@ -23,24 +28,6 @@ class byfn_cli_release_tests(unittest.TestCase):
                 shell=True, stderr=subprocess.STDOUT, stdout=logfile)
         logfile.close()
         self.assertEqual(returncode, 0, msg="test_byfn_cli_default_channel "
-                "tests are failed. \nPlease check the logfile "
-                +logfile.name+" for more details.")
-
-    def test_byfn_cli_custom_channel(self):
-        '''
-         In this cli test, we execute the byfn_cli tests on published release
-         docker images and pull published fabric binaries and perform tests on
-         fabric-samples repository.
-
-         Passing criteria: test_byfn_cli_custom_channel test completed successfully with
-         exit code 0
-        '''
-        logfile = open("output_byfn_cli_custom_channel.log", "w")
-        returncode = subprocess.call(
-                "././run_byfn_cli_release_tests.sh byfnchannel",
-                shell=True, stderr=subprocess.STDOUT, stdout=logfile)
-        logfile.close()
-        self.assertEqual(returncode, 0, msg="test_byfn_cli_custom_channel "
                 "tests are failed. \nPlease check the logfile "
                 +logfile.name+" for more details.")
 
