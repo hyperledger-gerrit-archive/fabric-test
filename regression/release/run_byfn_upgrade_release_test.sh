f0d05fb6e538d3fac94428887afe9cf9d0d672b7
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

---
+++ regression/release/run_byfn_upgrade_release_test.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -ue
+#
+# SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+WD="${GOPATH}/src/github.com/hyperledger/fabric-samples"
+REPO_NAME=fabric-samples
+NEXUS_REPO_URL=https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric
+
+clean_directory() {
+  rm -rf $WD
+}
+
+clone_repo() {
+  git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME \
+  $WD
+  cd $WD
+
+  # Display the RELEASE_VERSION to indicate the RELEASE being tested.
+  echo "--------> RELEASE_VERSION : $RELEASE_VERSION"
+}
+
+run_upgrade_test() {
+  echo "############### BYFN UPGRADE TEST ################"
+  echo "#######################################################"
+  mkdir -p $WD/bin
+  cd $WD/bin
+  wget $NEXUS_REPO_URL/linux-$ARCH-$FABRIC_PREVIOUS_VERSION/hyperledger-fabric-linux-$ARCH-$FABRIC_PREVIOUS_VERSION.tar.gz | tar xz
+  cd $WD/first-network
+  git checkout v$FABRIC_PREVIOUS_VERSION
+  echo y | ./byfn.sh up -i $FABRIC_PREVIOUS_VERSION
+  git checkout master
+  echo y | ./byfn.sh upgrade
+  echo y | ./byfn.sh -m down
+}
+
+main() {
+  run_upgrade_test
+}
+
+main
