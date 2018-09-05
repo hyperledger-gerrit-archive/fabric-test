b69b70a0f624259048e0a2a7e32099eb3ccf24c1
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

--- regression/release/run_byfn_cli_release_tests.sh
+++ regression/release/run_byfn_cli_release_tests.sh
@@ -1,41 +1,103 @@
-#!/bin/bash
-#
-# Copyright IBM Corp. All Rights Reserved.
+#!/bin/bash -ue
 #
 # SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
 #
-
-# RUN BYFN Test
-#####################
-
-CH_NAME="$1"
-rm -rf ${GOPATH}/src/github.com/hyperledger/fabric-samples
-
-WD="${GOPATH}/src/github.com/hyperledger/fabric-samples"
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 REPO_NAME=fabric-samples
+WD="${WORKSPACE}/src/github.com/hyperledger/${REPO_NAME}"
+BRANCH=${GERRIT_BRANCH:=master}
 
-git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
-cd $WD
+clean_directory() {
+  rm -rf $WD
+}
 
-curl -L https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap-1.0.0.sh -o bootstrap-1.0.0.sh
-chmod +x bootstrap-1.0.0.sh
-./bootstrap-1.0.0.sh
+clone_repo() {
 
-cd $WD/first-network
-export PATH=$WD/bin:$PATH
-echo y | ./byfn.sh -m down
+  git clone --single-branch -b $BRANCH \
+    git://cloud.hyperledger.org/mirror/$REPO_NAME $WD
 
-     if [ -z "${CH_NAME}" ]; then
-          echo "Generating artifacts for default channel"
-          echo y | ./byfn.sh -m generate
-	  echo "setting to default channel 'mychannel'"
-          echo y | ./byfn.sh -m up -t 10
-          echo
-      else
-          echo "Generate artifacts for custom Channel"
-          echo y | ./byfn.sh -m generate -c $CH_NAME
-          echo "Setting to non-default channel $CH_NAME"
-          echo y | ./byfn.sh -m up -c $CH_NAME -t 10
-          echo
-     fi
-echo y | ./byfn.sh -m down
+  cd $WD
+  git checkout $FAB_SAMPLES_REL_COMMIT
+
+}
+
+run_tests() {
+
+  pushd $WD/first-network
+
+  echo "############## BYFN,EYFN DEFAULT CHANNEL TEST ###########"
+  echo "#########################################################"
+
+  echo y | ./byfn.sh -m down
+  echo y | ./byfn.sh -m generate
+  echo y | ./byfn.sh -m up -t 100
+
+  echo y | ./eyfn.sh -m up -t 100
+  echo y | ./eyfn.sh -m down
+  echo
+  echo "############## BYFN,EYFN CUSTOM CHANNEL TEST ############"
+  echo "#########################################################"
+  echo y | ./byfn.sh -m generate -c fabricrelease
+  echo y | ./byfn.sh -m up -c fabricrelease -t 100
+  echo y | ./eyfn.sh -m up -c fabricrelease -t 100
+  echo y | ./eyfn.sh -m down
+  echo
+  echo "############# BYFN,EYFN CUSTOM CHANNEL WITH COUCHDB TEST ##############"
+  echo "#######################################################################"
+  echo y | ./byfn.sh -m generate -c fabricrelease-couchdb
+  echo y | ./byfn.sh -m up -c fabricrelease-couchdb -s couchdb -t 100 -d 15
+  echo y | ./eyfn.sh -m up -c fabricrelease-couchdb -s couchdb -t 100 -d 15
+  echo y | ./eyfn.sh -m down
+  echo
+  echo "############### BYFN,EYFN WITH NODE Chaincode. TEST ################"
+  echo "####################################################################"
+  echo y | ./byfn.sh -m up -l node -t 100
+  echo y | ./eyfn.sh -m up -l node -t 100
+  echo y | ./eyfn.sh -m down
+
+  popd
+}
+
+function clearContainers() {
+
+  CONTAINER_IDS=$(docker ps -aq)
+
+  if [ -z "$CONTAINER_IDS" ] || [ "$CONTAINER_IDS" = " " ]; then
+    echo "---- No containers available for deletion ----"
+  else
+    docker rm -f $CONTAINER_IDS || true
+    echo "---- Docker containers after cleanup ----"
+    docker ps -a
+  fi
+}
+
+function removeUnwantedImages() {
+  DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" \
+  | awk '{print $3}')
+
+  if [ -z "$DOCKER_IMAGE_IDS" ] || [ "$DOCKER_IMAGE_IDS" = " " ]; then
+    echo "---- No images available for deletion ----"
+  else
+    docker rmi -f $DOCKER_IMAGE_IDS || true
+    echo "---- Docker images after cleanup ----"
+    docker images
+  fi
+}
+
+main() {
+  clean_directory
+  clone_repo
+  # Copy the binaries from fabric-test
+  cp -r ${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-test/regression/release/fabric-samples/bin/ .
+  run_tests
+  clearContainers
+  removeUnwantedImages
+}
+
+main
--- regression/release/run_e2e_node_sdk.sh
+++ regression/release/run_e2e_node_sdk.sh
@@ -1,28 +1,101 @@
-#!/bin/bash
-
-#
-# Copyright IBM Corp. All Rights Reserved.
+#!/bin/bash -ue
 #
 # SPDX-License-Identifier: Apache-2.0
-#     
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+WD="${WORKSPACE}/src/github.com/hyperledger/fabric-sdk-node"
+BRANCH=${GERRIT_BRANCH:=master}
 
-rm -rf $GOPATH/src/github.com/hyperledger/fabric-sdk-node
+clean_directory() {
+  rm -rf $WD
+}
 
-WD="$GOPATH/src/github.com/hyperledger/fabric-sdk-node"
-SDK_REPO_NAME=fabric-sdk-node
-git clone https://github.com/hyperledger/$SDK_REPO_NAME $WD
-cd $WD
-git checkout tags/v1.0.0
-cd test/fixtures
-docker rm -f "$(docker ps -aq)" || true
-docker-compose up >> node_dockerlogfile.log 2>&1 &
-sleep 10
-docker ps -a
-cd ../.. && npm install
-npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
-gulp || true
-gulp ca || true
-rm -rf node_modules/fabric-ca-client && npm install
-gulp test
+clone_repo()
 
-docker rm -f "$(docker ps -aq)" || true
+  git clone --single-branch -b $BRANCH \
+    https://github.com/hyperledger/fabric-sdk-node $WD
+
+  cd $WD
+  git checkout $FABRIC_SDK_NOD_REL_COMMIT
+
+}
+
+run_gulp_tests() {
+  pushd $WD
+  wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+  # shellcheck source=/dev/null
+  export NVM_DIR="$HOME/.nvm"
+  # shellcheck source=/dev/null
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+  # Install nodejs version 8.11.3
+  nvm install 8.11.3
+
+  # use nodejs 8.11.3 version
+  nvm use --delete-prefix v8.11.3 --silent
+
+  echo "npm version ======>"
+  npm -v
+  echo "node version =======>"
+  node -v
+
+  pushd $WD/test/fixtures
+  cat docker-compose.yaml > docker-compose.log
+  docker-compose up >> dockerlogfile.log 2>&1 &
+  sleep 10
+  docker ps -a
+  pushd ../..
+  npm install
+  npm config set prefix ~/npm
+  npm install -g gulp
+  npm install -g istanbul
+  gulp
+  gulp ca
+  rm -rf node_modules/fabric-ca-client
+  npm install
+  istanbul cover --report cobertura test/integration/e2e.js
+
+  popd
+}
+
+function clearContainers() {
+
+  CONTAINER_IDS=$(docker ps -aq)
+
+  if [ -z "$CONTAINER_IDS" ] || [ "$CONTAINER_IDS" = " " ]; then
+    echo "---- No containers available for deletion ----"
+  else
+    docker rm -f $CONTAINER_IDS || true
+    echo "---- Docker containers after cleanup ----"
+    docker ps -a
+  fi
+}
+
+function removeUnwantedImages() {
+  DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" \
+  | awk '{print $3}')
+
+  if [ -z "$DOCKER_IMAGE_IDS" ] || [ "$DOCKER_IMAGE_IDS" = " " ]; then
+    echo "---- No images available for deletion ----"
+  else
+    docker rmi -f $DOCKER_IMAGE_IDS || true
+    echo "---- Docker images after cleanup ----"
+    docker images
+  fi
+}
+
+function main() {
+  clean_directory
+  clone_repo
+  run_gulp_tests
+  clearContainers
+  removeUnwantedImages
+}
+
+main
