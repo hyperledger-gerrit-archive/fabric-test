8bb73756580805bdc413f562df8f4b3c6ae899ff
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

--- regression/release/run_e2e_java_sdk.sh
+++ regression/release/run_e2e_java_sdk.sh
@@ -1,21 +1,41 @@
-#!/bin/bash
-
-#
-# Copyright IBM Corp. All Rights Reserved.
+#!/bin/bash -ue
 #
 # SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
 #
-# Test Java SDK e2e tests
-#
-
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 WD="${GOPATH}/src/github.com/hyperledger/fabric-sdk-java"
-#WD="${WORKSPACE}/gopath/src/github.com/hyperledger/fabric-sdk-java"
-SDK_REPO_NAME=fabric-sdk-java
-git clone https://github.com/hyperledger/fabric-sdk-java $WD
-cd $WD
-git checkout tags/v1.0.0
-export GOPATH=$WD/src/test/fixture
 
-cd $WD/src/test
-chmod +x cirun.sh
-source cirun.sh
+clean_directory() {
+  rm -rf $WD
+}
+
+clone_repo() {
+  git clone https://github.com/hyperledger/fabric-sdk-java $WD
+
+  pushd $WD
+
+  # checkout to latest release commit
+  echo "--------> RELEASE_VERSION : $RELEASE_VERSION"
+  git checkout $RELEASE_COMMIT
+}
+
+run_e2e_java_tests() {
+  export GOPATH=$WD/src/test/fixture
+  pushd $WD/src/test
+  ./cirun.sh
+  popd
+}
+
+main() {
+  clean_directory
+  clone_repo
+  run_e2run_e2e_java_tests
+}
+
+main
