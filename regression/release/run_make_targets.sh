cda33caad010f598a57119e6a74f4f970bca6f82
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

--- regression/release/run_make_targets.sh
+++ regression/release/run_make_targets.sh
@@ -1,28 +1,29 @@
-#!/bin/bash
-
+#!/bin/bash  -e
+set -o pipefail
 #
 # Copyright IBM Corp. All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+rm -rf ${GOPATH}/src/github.com/hyperledger/fabric
 
-set -o pipefail
+WD="${GOPATH}/src/github.com/hyperledger/fabric"
+REPO_NAME=fabric
 
-CWD=$GOPATH/src/github.com/hyperledger/fabric
-cd $CWD
+git clone ssh://hyperledger-jobbuilder@gerrit.hyperledger.org:29418/$REPO_NAME $WD
+cd $WD
 
-VERSION=`cat Makefile | grep BASE_VERSION | awk '{print $3}' | head -n1`
-echo "===>Release_VERSION: $VERSION"
+#RELEASE_VERSION=$(cat Makefile | grep "BASE_VERSION =" | awk '{print $3}')
+echo "--------> RELEASE_VERSION : $RELEASE_VERSION"
+git checkout tags/v$RELEASE_VERSION
 
 makeCleanAll() {
-
   make clean-all
   echo "clean-all from fabric repository"
- }
+}
 
 # make native
 makeNative() {
-
   make native
   for binary in chaintool configtxgen configtxlator cryptogen orderer peer; do
   	if [ ! -f $CWD/build/bin/$binary ] ; then
@@ -37,67 +38,75 @@ makeNative() {
 
 # Build peer, orderer, configtxgen, cryptogen and configtxlator
 makeBinary() {
+  make clean-all
 
-   make clean-all
-   make peer && make orderer && make configtxgen && make cryptogen && make configtxlator
-   for binary in peer orderer configtxgen cryptogen configtxlator; do
-         if [ ! -f $CWD/build/bin/$binary ] ; then
-     	   echo " ====> ERROR !!! $binary is not available"
-     	   echo
-           exit 1
-        fi
+  for image in configtxgen configtxlator cryptogen discover idemixgen orderer \
+  peer; do
+   make $image
+  done
 
-           echo " ====> PASS !!! $binary is available"
-   done
+  for binary in peer orderer configtxgen cryptogen configtxlator; do
+   if [ ! -f $WD/.build/bin/$binary ] ; then
+     echo " ====> ERROR !!! $binary is not available"
+     echo
+     exit 1
+  fi
+
+  echo " ====> PASS !!! $binary is available"
+  done
 }
 
 # Create tar files for each platform
 makeDistAll() {
+  make clean-all
+  make dist-all
 
-   make clean-all
-   make dist-all
-   for dist in linux-amd64 windows-amd64 darwin-amd64 linux-ppc64le linux-s390x; do
-        if [ ! -d $CWD/release/$dist ] ; then
-     	   echo " ====> ERROR !!! $dist is not available"
-     	   echo
-           exit 1
-        fi
+  for dist in linux-amd64 windows-amd64 darwin-amd64 linux-ppc64le \
+  linux-s390x; do
+    if [ ! -d $CWD/release/$dist ] ; then
+      echo " ====> ERROR !!! $dist is not available"
+  	  echo
+      exit 1
+    fi
 
-           echo " ====> PASS !!! $dist is available"
-done
-}
-
-# Create docker images
-makeDocker() {
-    make docker-clean
-    make docker
-        if [ $? -ne 0 ] ; then
-           echo " ===> ERROR !!! Docker Images are not available"
-           echo
-           exit 1
-        fi
-           echo " ===> PASS !!! Docker Images are available"
+    echo " ====> PASS !!! $dist is available"
+  done
 }
 
 # Verify the version built in peer and configtxgen binaries
 makeVersion() {
-    make docker-clean
-    make release
-    cd release/linux-amd64/bin
-    ./peer --version > peer.txt
-    Pversion=$(grep -v "2017" peer.txt | grep Version: | awk '{print $2}' | head -n1)
-        if [ "$Pversion" != "$VERSION" ]; then
-           echo " ===> ERROR !!! Peer Version check failed"
-           echo
-        fi
-   ./configtxgen --version > configtxgen.txt
-   Configtxgen=$(grep -v "2017" configtxgen.txt | grep Version: | awk '{print $2}' | head -n1)
-        if [ "$Configtxgen" != "$VERSION" ]; then
-           echo "====> ERROR !!! configtxgen Version check failed:"
-           echo
-           exit 1
-        fi
-           echo "====> PASS !!! Configtxgen version verified:"
-}
+  make docker-clean
+  make release
+  cd release/linux-amd64/bin
+  ./peer version > peer.txt
+  Pversion=$(grep -v "Version" peer.txt | grep Version: | awk '{print $2}' | head -n1)
+      if [ "$Pversion" != "$RELEASE_VERSION" ]; then
+         echo " ===> ERROR !!! Peer Version check failed"
+         echo
+      fi
+ ./configtxgen --version > configtxgen.txt
+ Configtxgen=$(grep -v "Version" configtxgen.txt | grep Version: | awk '{print $2}' | head -n1)
+      if [ "$Configtxgen" != "$RELEASE_VERSION" ]; then
+         echo "====> ERROR !!! configtxgen Version check failed:"
+         echo
+         exit 1
+      fi
+         echo "====> PASS !!! Configtxgen version verified:"
+ ./orderer --version > orderer.txt
+ orderer=$(grep -v "Version" orderer.txt | grep Version: | awk '{print $2}' | head -n1)
+      if [ "$orderer" != "$RELEASE_VERSION" ]; then
+         echo "====> ERROR !!! orderer Version check failed:"
+         echo
+         exit 1
+      fi
+         echo "====> PASS !!! orderer version verified:"
 
-$1
+ ./configtxlator --version > configtxlator.txt
+ configtxlator=$(grep -v "Version" configtxlator.txt | grep Version: | awk '{print $2}' | head -n1)
+      if [ "$configtxlator" != "$RELEASE_VERSION" ]; then
+         echo "====> ERROR !!! configtxlator Version check failed:"
+         echo
+         exit 1
+      fi
+         echo "====> PASS !!! configtxlator version verified:"
+}
