c8c558e0a2b193ee59a80eb08480b90ef02ea669
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

--- regression/release/run_e2e_node_sdk.sh
+++ regression/release/run_e2e_node_sdk.sh
@@ -1,28 +1,53 @@
-#!/bin/bash
-
-#
-# Copyright IBM Corp. All Rights Reserved.
+#!/bin/bash -e
 #
 # SPDX-License-Identifier: Apache-2.0
-#     
-
-rm -rf $GOPATH/src/github.com/hyperledger/fabric-sdk-node
-
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
 WD="$GOPATH/src/github.com/hyperledger/fabric-sdk-node"
 SDK_REPO_NAME=fabric-sdk-node
-git clone https://github.com/hyperledger/$SDK_REPO_NAME $WD
-cd $WD
-git checkout tags/v1.0.0
-cd test/fixtures
-docker rm -f "$(docker ps -aq)" || true
-docker-compose up >> node_dockerlogfile.log 2>&1 &
-sleep 10
-docker ps -a
-cd ../.. && npm install
-npm config set prefix ~/npm && npm install -g gulp && npm install -g istanbul
-gulp || true
-gulp ca || true
-rm -rf node_modules/fabric-ca-client && npm install
-gulp test
 
-docker rm -f "$(docker ps -aq)" || true
+clean_directory() {
+  rm -rf $GOPATH/src/github.com/hyperledger/fabric-sdk-node
+}
+
+clone_repo() {
+  git clone https://github.com/hyperledger/$SDK_REPO_NAME $WD
+}
+
+run_gulp_tests() {
+  pushd $WD
+
+  echo "--------> RELEASE_VERSION : $RELEASE_VERSION"
+  git checkout $RELEASE_COMMIT
+
+  npm install
+  npm config set prefix ~/npm
+  npm install -g gulp
+  npm install -g istanbul
+  gulp
+  gulp ca
+  rm -rf node_modules/fabric-ca-client
+  npm install
+  gulp test
+
+  popd
+}
+
+remove_test_image(){
+  docker rm -f "$(docker ps -aq)" || true
+}
+
+main() {
+  clean_directory
+  clone_repo
+  run_gulp_tests
+  remove_test_image
+}
+
+main
