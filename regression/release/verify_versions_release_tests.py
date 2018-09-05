a44ac48f6e551ff0a6915959e5b8eaf995f8c4fc
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-09-04 21:44:27 -0400

    [FABCI-32] updates release tests
    This patch updates the release testsuite in fabric-test repo to use
    them in CI release tests. All these tests use the latest released
    docker images and binaries.
    
    Replaces: Change-ID 18339
    
    Change-Id: Iad4b74c3e693058c95036181c03325e7f39ddb91
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

---
+++ regression/release/verify_versions_release_tests.py
@@ -0,0 +1,29 @@
+#
+# SPDX-License-Identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2018 IBM Corporation, The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License 2.0
+# which accompanies this distribution, and is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import unittest
+import subprocess
+
+class verify_versions(unittest.TestCase):
+    def test_verifyVersion(self):
+        '''
+         In this make targets test, we execute version check to make sure binaries version
+         is correct.
+
+         Passing criteria: make version test completed successfully with
+         exit code 0
+        '''
+        logfile = open("output_verify_version_release_tests.log", "w")
+        returncode = subprocess.call(
+                "./run_verify_versions.sh verify_version",
+                shell=True, stderr=subprocess.STDOUT, stdout=logfile)
+        logfile.close()
+        self.assertEqual(returncode, 0, msg="Run make version target "
+                "verify versions tests failed. \nPlease check the logfile ")
