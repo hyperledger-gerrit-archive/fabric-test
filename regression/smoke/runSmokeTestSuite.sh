2fd2122066be8354163fb8c890ff843496fc8e18
Scott Zwierzynski <scottz@us.ibm.com>
2018-09-06 11:50:44 -0400

    [FAB-11898] archive data for Testviewer WIP
    Fix the daily/runPte and daily/runLte scripts to
    correctly and consistently archive all the daily
    test suites result logfiles for PTE, OTE, and
    LTE tests from jenkins into Nexus.
    
    Apparently some output log files had been renamed
    recently, so they will now be archived from those
    new correct output locations in jenkins to the
    new standardized archive locations in Nexus by
    the fabric-test/regression/daily/run* scripts,
    
    Also this fixes the TestViewer tool metrics.js to
    retrieve test data (up to 6 months) from the NEW
    archived locations in Nexus.
    
    With https://gerrit.hyperledger.org/r/#/c/26198/
    also being fixed today, along with another change
    last week to produce a new pte report summary
    file, the PTE results formats have changed - plus
    the output filenames of the CI automated PTE
    tests have changed to
    CITest/Logs/$TESTCASE-pteReport.log. That is why
    now we are editing the test suite script files
    smoke/systest_pte.py and daily/systest_pte.py to
    check for new result strings in new log file names.
    
    Change-Id: I9cb34ce090e932f697c82d7c8ee247f65da58021
    Signed-off-by: Scott Zwierzynski <scottz@us.ibm.com>

--- regression/smoke/runSmokeTestSuite.sh
+++ regression/smoke/runSmokeTestSuite.sh
@@ -9,14 +9,14 @@ cd $SMOKEDIR
 
 echo "======== Behave feature and system tests ========"
 cd ../../feature
-behave --junit --junit-directory ../regression/smoke/. --tags=-skip --tags=smoke -k -D logs=y
+###behave --junit --junit-directory ../regression/smoke/. --tags=-skip --tags=smoke -k -D logs=y
 cd -
 
 echo "======== Ledger component performance tests using LTE ========"
-py.test -v --junitxml results_ledger_lte_smoke.xml ledger_lte_smoke.py
+###py.test -v --junitxml results_ledger_lte_smoke.xml ledger_lte_smoke.py
 
 echo "======== Orderer component test using OTE and NL tools ========"
-py.test -v --junitxml results_orderer_ote.xml orderer_ote.py
+###py.test -v --junitxml results_orderer_ote.xml orderer_ote.py
 
 echo "======== Performance Test using PTE and NL tools ========"
 cd $GOPATH/src/github.com/hyperledger/fabric-test/tools/PTE
@@ -29,4 +29,10 @@ else
     echo "Successfully installed npm."
 fi
 cd $SMOKEDIR && py.test -v --junitxml results_systest_pte.xml systest_pte.py
+
+
+### WIP: Remove the behave/LTE/OTE tests above, and below run the PTE DAILY tests too
+echo "Done executing pte smoke"
+cd "$GOPATH/src/github.com/hyperledger/fabric-test/regression/daily" && py.test -v --junitxml results_systest_pte_daily.xml systest_pte.py && echo "------> PTE daily tests completed"
+cd -
 
