d1ab2c27f25497193cb42a016134fa92e0511937
Matthias Neugschwandtner <eug@zurich.ibm.com>
2018-09-10 10:37:14 +0200

    [FAB-11921] SBE chaincode for SVT
    A chaincode for SVT of state-based endorsement.
    
    Change-Id: I1d64dabc909611a7b64e82c6c15b37a7aaad8566
    Signed-off-by: Matthias Neugschwandtner <eug@zurich.ibm.com>
    Signed-off-by: Alessandro Sorniotti <ale.linux@sopit.net>

---
+++ chaincodes/sbe/chaincode.go
@@ -0,0 +1,134 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package sbe
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/core/chaincode/shim/ext/statebased"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+type Record struct {
+	Key   string
+	Value []byte
+	Orgs  []string
+}
+
+type EndorsementCC struct {
+}
+
+// Init callback
+func (cc *EndorsementCC) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
+}
+
+// Invoke dispatcher
+func (cc *EndorsementCC) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	funcName, _ := stub.GetFunctionAndParameters()
+	if function, ok := functions[funcName]; ok {
+		return function(stub)
+	}
+	return shim.Error(fmt.Sprintf("Unknown function %s", funcName))
+}
+
+// function dispatch map used by Invoke()
+var functions = map[string]func(stub shim.ChaincodeStubInterface) pb.Response{
+	"updateRecordVal": updateRecordValue,
+	"updateRecordEP":  updateRecordEP,
+	"getRecord":       getRecord,
+}
+
+func updateRecordEP(stub shim.ChaincodeStubInterface) pb.Response {
+	_, parameters := stub.GetFunctionAndParameters()
+	if len(parameters) < 2 {
+		return shim.Error("Wrong number of arguments supplied.")
+	}
+
+	// set the EP
+	err := setEP(stub, parameters[0], parameters[1:]...)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success([]byte{})
+}
+
+func updateRecordValue(stub shim.ChaincodeStubInterface) pb.Response {
+	_, parameters := stub.GetFunctionAndParameters()
+	if len(parameters) != 2 {
+		return shim.Error("Wrong number of arguments supplied.")
+	}
+
+	// set the value
+	err := stub.PutState(parameters[0], []byte(parameters[1]))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success([]byte{})
+}
+
+func setEP(stub shim.ChaincodeStubInterface, key string, orgs ...string) error {
+	ep, err := statebased.NewStateEP(nil)
+	if err != nil {
+		return err
+	}
+	// add organizations to endorsement policy
+	err = ep.AddOrgs(statebased.RoleTypePeer, orgs...)
+	if err != nil {
+		return err
+	}
+	epBytes, err := ep.Policy()
+	if err != nil {
+		return err
+	}
+	// set the endorsement policy for the key
+	err = stub.SetStateValidationParameter(key, epBytes)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func getRecord(stub shim.ChaincodeStubInterface) pb.Response {
+	_, parameters := stub.GetFunctionAndParameters()
+	if len(parameters) != 1 {
+		return shim.Error("Wrong number of arguments supplied.")
+	}
+	key := parameters[0]
+
+	// get the endorsement policy for the key
+	epBytes, err := stub.GetStateValidationParameter(key)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	ep, err := statebased.NewStateEP(epBytes)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// get the value of the key
+	val, err := stub.GetState(key)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// put it into the json
+	r := &Record{
+		Key:   key,
+		Value: val,
+		Orgs:  ep.ListOrgs(),
+	}
+	rBytes, err := json.Marshal(r)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(rBytes)
+}
