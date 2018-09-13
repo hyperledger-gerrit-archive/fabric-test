ca1235c564233a47ddb729e6c9317227eab4a34d
Sambhav Nidamarty <sambhavdutt@gmail.com>
2018-09-12 23:04:21 -0400

    FAB-11978 Pull the javaenv for smoke & daily tests
    This change is to get the javaenv image in the
    smoke tests, pull the javaenv image from Nexus
    in the daily tests for the master branch.
    
    Change-Id: Iab6f88b2a8f18f0265ab27e1248c22963de9b0ea
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

--- scripts/pullDockerImages.sh
+++ scripts/pullDockerImages.sh
@@ -60,6 +60,25 @@ echo
 docker images | grep "hyperledger*"
 echo
 
+#####################################################
+# Pull the fabric-chaincode-javaenv image from Nexus
+#####################################################
+if [ "$GERRIT_BRANCH" != "master" ]; then
+       echo "========> SKIP: javaenv image is not available on $GERRIT_BRANCH"
+else
+       NEXUS_URL=nexus3.hyperledger.org:10001
+       ORG_NAME="hyperledger/fabric"
+       IMAGE=javaenv
+       : ${STABLE_VERSION:=amd64-latest}
+       docker pull $NEXUS_URL/$ORG_NAME-$IMAGE:$STABLE_VERSION
+       docker tag $NEXUS_URL/$ORG_NAME-$IMAGE:$STABLE_VERSION $ORG_NAME-$IMAGE
+       docker tag $NEXUS_URL/$ORG_NAME-$IMAGE:$STABLE_VERSION $ORG_NAME-$IMAGE:amd64-1.3.0
+       docker tag $NEXUS_URL/$ORG_NAME-$IMAGE:$STABLE_VERSION $ORG_NAME-$IMAGE:amd64-latest
+       ######################################
+       docker images | grep hyperledger/fabric-javaenv || true
+fi
+echo
+
 echo "======== PULL FABRIC BINARIES ========"
 echo "------------> RELEASE_COMMIT:" $RELEASE_COMMIT
 RELEASE_COMMIT=${RELEASE_COMMIT:0:7}
