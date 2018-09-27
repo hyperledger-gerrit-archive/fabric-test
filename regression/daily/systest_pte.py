e594debf77b3a0604d9b7aa9544421d8514c81d6
Scott Zwierzynski <scottz@us.ibm.com>
2018-09-27 16:52:18 -0400

    [FAB-8192] add regr tests to pte daily suite - WIP
    FAB-8192 marbles02 couchdb 4 threads x 1000 invokes
    FAB-8199 marbles02 queries: readMarble
    FAB-8200 marbles02 rich queries: queryMarblesByOwner
    FAB-8201 marbles02 rich queries: queryMarbles
    FAB-7329 samplejs channel events
    FAB-7333 samplejs filtered block events
    FAB-7647 samplecc single thread TX latency
    
    Change-Id: Id2d322ce256f4d8cc9fc8f7f9eb6ac0f236b1273
    Signed-off-by: Scott Zwierzynski <scottz@us.ibm.com>

--- regression/daily/systest_pte.py
+++ regression/daily/systest_pte.py
@@ -13,13 +13,21 @@ import subprocess
 
 TEST_PASS_STRING="RESULT=PASS"
 
-######################################################################
-### COUCHDB
-######################################################################
-
 logs_directory = '../../tools/PTE/CITest/Logs'
 scenarios_directory = '../../tools/PTE/CITest/scenarios'
 nl_directory = '../../tools/NL'
+
+testScriptFailed =      "Test Failed with non-zero exit code; check for errors in fabric-test/tools/PTE/CITest/Logs/"
+noTxSummary =           "Error: pteReport.log does not contain INVOKE Overall transactions"
+invokeFailure =         "Error: incorrect number of INVOKE transactions sent or received"
+invokeSendFailure =     "Error sending INVOKE proposal to peer or sending broadcast transaction to orderer"
+eventReceiveFailure =   "Error: event receive failure: INVOKE TX events arrived late after eventOpt.timeout, and/or transaction events were never received"
+queryCountFailure =     "Error: incorrect number of QUERY transactions sent or received"
+
+
+######################################################################
+### COUCHDB
+######################################################################
 
 class Perf_Stress_CouchDB(unittest.TestCase):
 
@@ -55,33 +63,40 @@ class Perf_Stress_CouchDB(unittest.TestCase):
 
         # Run the test scenario: launch network and run the invokes and query tests.
         returncode = subprocess.call("./FAB-3833-2i.sh", cwd=scenarios_directory, shell=True)
-        self.assertEqual(returncode, 0, msg="Test Failed with non-zero exit code; check for errors in fabric-test/tools/PTE/CITest/Logs/")
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
         # tear down the network, including all the nodes docker containers
         returncode = subprocess.call("./networkLauncher.sh -a down", cwd=nl_directory, shell=True)
 
-        # First check if the test finished and created the report file; then check it for accurate counts
+        # check if the test created the report file
         logfilelist = subprocess.check_output("ls", cwd=logs_directory, shell=True)
         self.assertIn("FAB-3833-2i-pteReport.log", logfilelist)
 
+        # check if the test finished and printed the Overall summary
+        count = subprocess.check_output(
+                "grep \"INVOKE Overall transactions:\" FAB-3833-2i-pteReport.log | wc -l",
+                cwd=logs_directory, shell=True)
+        self.assertEqual(int(count.strip()), 1, msg=noTxSummary)
+
+        # check the counts
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall transactions: sent 20000 received 20000\" FAB-3833-2i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=invokeFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall failures: proposal 0 transactions 0\" FAB-3833-2i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=invokeSendFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall event: received 20000 timeout 0 unreceived 0\" FAB-3833-2i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=eventReceiveFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT QUERY Overall transactions: sent 20000 received 20000\" FAB-3833-2i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=queryCountFailure)
 
 
     def test_FAB3832_4i_FAB3834_4q(self):
@@ -116,33 +131,71 @@ class Perf_Stress_CouchDB(unittest.TestCase):
 
         # Run the test scenario: launch network and run the invokes and query tests.
         returncode = subprocess.call("./FAB-3832-4i.sh", cwd=scenarios_directory, shell=True)
-        self.assertEqual(returncode, 0, msg="Test Failed with non-zero exit code; check for errors in fabric-test/tools/PTE/CITest/Logs/")
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
         # tear down the network, including all the nodes docker containers
         returncode = subprocess.call("./networkLauncher.sh -a down", cwd=nl_directory, shell=True)
 
-        # First check if the test finished and created the report file; then check it for accurate counts
+        # check if the test created the report file
         logfilelist = subprocess.check_output("ls", cwd=logs_directory, shell=True)
         self.assertIn("FAB-3832-4i-pteReport.log", logfilelist)
 
+        # check if the test finished and printed the Overall summary
+        count = subprocess.check_output(
+                "grep \"INVOKE Overall transactions:\" FAB-3832-4i-pteReport.log | wc -l",
+                cwd=logs_directory, shell=True)
+        self.assertEqual(int(count.strip()), 1, msg=noTxSummary)
+
+        # check the counts
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall transactions: sent 40000 received 40000\" FAB-3832-4i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=invokeFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall failures: proposal 0 transactions 0\" FAB-3832-4i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=invokeSendFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall event: received 40000 timeout 0 unreceived 0\" FAB-3832-4i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=eventReceiveFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT QUERY Overall transactions: sent 40000 received 40000\" FAB-3832-4i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=queryCountFailure)
+
+
+    def test_FAB8192_4i_marbles_FAB8199_4q_FAB8200_4q_FAB8201_4q(self):
+        '''
+        FAB-8192-4i marbles02 couchdb 4 threads x 1000 invokes (initMarble),
+            and 4 x 1000 (three types of queries):
+            FAB-8199-4q: 4 threads queries: readMarble
+            FAB-8200-4q: 4 threads rich queries: queryMarblesByOwner
+            FAB-8201-4q: 4 threads rich queries: queryMarbles
+        '''
+
+        # Run the test scenario: launch network and run the invokes and query tests.
+        returncode = subprocess.call("./FAB-8192-4i.sh", cwd=scenarios_directory, shell=True)
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
+        # tear down the network, including all the nodes docker containers
+        returncode = subprocess.call("./networkLauncher.sh -a down", cwd=nl_directory, shell=True)
+
+        # check if the test created the report file
+        logfilelist = subprocess.check_output("ls", cwd=logs_directory, shell=True)
+        self.assertIn("FAB-8192-4i-pteReport.log", logfilelist)
+        ### should we also check if other report files were created for the queries tests, or are they all included in the one?
+
+        # check if the test finished and printed the Overall summary
+        count = subprocess.check_output(
+                "grep \"INVOKE Overall transactions:\" FAB-8192-4i-pteReport.log | wc -l",
+                cwd=logs_directory, shell=True)
+        self.assertEqual(int(count.strip()), 1, msg=noTxSummary)
+
+        # check the counts
+        # count = subprocess.check_output(
+        #
 
 
 ######################################################################
@@ -183,33 +236,40 @@ class Perf_Stress_LevelDB(unittest.TestCase):
 
         # Run the test scenario: launch network and run the invokes and query tests.
         returncode = subprocess.call("./FAB-3808-2i.sh", cwd=scenarios_directory, shell=True)
-        self.assertEqual(returncode, 0, msg="Test Failed with non-zero exit code; check for errors in fabric-test/tools/PTE/CITest/Logs/")
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
         # tear down the network, including all the nodes docker containers
         returncode = subprocess.call("./networkLauncher.sh -a down", cwd=nl_directory, shell=True)
 
-        # First check if the test finished and created the report file; then check it for accurate counts
+        # check if the test created the report file
         logfilelist = subprocess.check_output("ls", cwd=logs_directory, shell=True)
         self.assertIn("FAB-3808-2i-pteReport.log", logfilelist)
 
+        # check if the test finished and printed the Overall summary
+        count = subprocess.check_output(
+                "grep \"INVOKE Overall transactions:\" FAB-3808-2i-pteReport.log | wc -l",
+                cwd=logs_directory, shell=True)
+        self.assertEqual(int(count.strip()), 1, msg=noTxSummary)
+
+        # check the counts
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall transactions: sent 20000 received 20000\" FAB-3808-2i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=invokeFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall failures: proposal 0 transactions 0\" FAB-3808-2i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=invokeSendFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall event: received 20000 timeout 0 unreceived 0\" FAB-3808-2i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=eventReceiveFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT QUERY Overall transactions: sent 20000 received 20000\" FAB-3808-2i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=queryCountFailure)
 
 
     def test_FAB3807_4i_FAB3835_4q(self):
@@ -244,32 +304,120 @@ class Perf_Stress_LevelDB(unittest.TestCase):
 
         # Run the test scenario: launch network and run the invokes and query tests.
         returncode = subprocess.call("./FAB-3807-4i.sh", cwd=scenarios_directory, shell=True)
-        self.assertEqual(returncode, 0, msg="Test Failed with non-zero exit code; check for errors in fabric-test/tools/PTE/CITest/Logs/")
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
         # tear down the network, including all the nodes docker containers
         returncode = subprocess.call("./networkLauncher.sh -a down", cwd=nl_directory, shell=True)
 
-        # First check if the test finished and created the report file; then check it for accurate counts
+        # check if the test created the report file
         logfilelist = subprocess.check_output("ls", cwd=logs_directory, shell=True)
         self.assertIn("FAB-3807-4i-pteReport.log", logfilelist)
 
+        # check if the test finished and printed the Overall summary
+        count = subprocess.check_output(
+                "grep \"INVOKE Overall transactions:\" FAB-3807-4i-pteReport.log | wc -l",
+                cwd=logs_directory, shell=True)
+        self.assertEqual(int(count.strip()), 1, msg=noTxSummary)
+
+        # check the counts
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall transactions: sent 40000 received 40000\" FAB-3807-4i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=invokeFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall failures: proposal 0 transactions 0\" FAB-3807-4i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=invokeSendFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT INVOKE Overall event: received 40000 timeout 0 unreceived 0\" FAB-3807-4i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=eventReceiveFailure)
 
         count = subprocess.check_output(
                 "grep \"CONSTANT QUERY Overall transactions: sent 40000 received 40000\" FAB-3807-4i-pteReport.log | wc -l",
                 cwd=logs_directory, shell=True)
-        self.assertEqual(int(count.strip()), 1)
+        self.assertEqual(int(count.strip()), 1, msg=queryCountFailure)
+
+
+    def test_FAB7329_4i_channel_events(self):
+        '''
+        FAB-7329 channel events, 1 ch NodeJS cc, 4 thrds
+        '''
+
+        # Run the test scenario: launch network and run the invokes and query tests.
+        returncode = subprocess.call("./FAB-7329-4i.sh", cwd=scenarios_directory, shell=True)
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
+        # tear down the network, including all the nodes docker containers
+        returncode = subprocess.call("./networkLauncher.sh -a down", cwd=nl_directory, shell=True)
+
+        # check if the test created the report file
+        logfilelist = subprocess.check_output("ls", cwd=logs_directory, shell=True)
+        self.assertIn("FAB-7329-4i-pteReport.log", logfilelist)
+        ### should we also check if other report files were created for the queries tests, or are they all included in the one?
+
+        # check if the test finished and printed the Overall summary
+        count = subprocess.check_output(
+                "grep \"INVOKE Overall transactions:\" FAB-7329-4i-pteReport.log | wc -l",
+                cwd=logs_directory, shell=True)
+        self.assertEqual(int(count.strip()), 1, msg=noTxSummary)
+
+        # check the counts
+        # count = subprocess.check_output(
+        #
+
+
+    def test_FAB7333_4i_filtered_block_events(self):
+        '''
+        FAB-7333 filtered block events, 1 ch NodeJS cc, 4 thrds
+        '''
+
+        # Run the test scenario: launch network and run the invokes and query tests.
+        returncode = subprocess.call("./FAB-7333-4i.sh", cwd=scenarios_directory, shell=True)
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
+        # tear down the network, including all the nodes docker containers
+        returncode = subprocess.call("./networkLauncher.sh -a down", cwd=nl_directory, shell=True)
+
+        # check if the test created the report file
+        logfilelist = subprocess.check_output("ls", cwd=logs_directory, shell=True)
+        self.assertIn("FAB-7333-4i-pteReport.log", logfilelist)
+        ### should we also check if other report files were created for the queries tests, or are they all included in the one?
+
+        # check if the test finished and printed the Overall summary
+        count = subprocess.check_output(
+                "grep \"INVOKE Overall transactions:\" FAB-7333-4i-pteReport.log | wc -l",
+                cwd=logs_directory, shell=True)
+        self.assertEqual(int(count.strip()), 1, msg=noTxSummary)
+
+        # check the counts
+        # count = subprocess.check_output(
+        #
+
+
+    def test_FAB7647_1i_latency(self):
+        '''
+        FAB-7647-1i.sh latency for single blocking thread, 1 transaction at a time
+        '''
+
+        # Run the test scenario: launch network and run the invokes and query tests.
+        returncode = subprocess.call("./FAB-7647-1i.sh", cwd=scenarios_directory, shell=True)
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
+        # tear down the network, including all the nodes docker containers
+        returncode = subprocess.call("./networkLauncher.sh -a down", cwd=nl_directory, shell=True)
+
+        # check if the test created the report file
+        logfilelist = subprocess.check_output("ls", cwd=logs_directory, shell=True)
+        self.assertIn("FAB-7647-1i-pteReport.log", logfilelist)
+        ### should we also check if other report files were created for the queries tests, or are they all included in the one?
+
+        # check if the test finished and printed the Overall summary
+        count = subprocess.check_output(
+                "grep \"INVOKE Overall transactions:\" FAB-7647-1i-pteReport.log | wc -l",
+                cwd=logs_directory, shell=True)
+        self.assertEqual(int(count.strip()), 1, msg=noTxSummary)
+
+        # check the counts
+        # count = subprocess.check_output(
+        #
 
 
