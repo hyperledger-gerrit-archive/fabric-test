241ef9cc7ce318f72833e59331684e179dc2079e
ratnakar <asara.ratnakar@gmail.com>
2018-10-05 00:06:05 -0400

    [FAB-12316] Cloud test automation
    * renamed regression to testSuites
    * Add cloud tests and tools under testSuites
    
    Change-Id: Id01280f1361e4bf664fec8868ce64bae3e2367ba
    Signed-off-by: ratnakar <asara.ratnakar@gmail.com>

---
+++ testSuites/cloud/shortSanity.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+# run short test suite on IBM cloud
+./runTestPTE.sh --cloud IBM --npm --cprof -i -t FAB-3808-2i FAB-3811-2q
