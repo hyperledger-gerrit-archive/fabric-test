8070fd5416b116d1526cc4c449021ccfacfd6993
vijaypunugubati <vijaypunugubati@gmail.com>
2019-02-09 17:44:09 -0500

    FAB-14342 Update version in fabric test scripts
    - This patch will change the pull version as 1.4.1-stable
    in release-1.4 branch in dockerpullimages macro for fabric-test.
    - Make changes in makefile to maintain version consistent in
    fabric-test as fabric repo.
    
    Change-Id: Ibe786374d0e6c8c8406b5c37394fd4c86ec023d8
    Signed-off-by: vijaypunugubati <vijaypunugubati@gmail.com>

--- scripts/pullDockerImages.sh
+++ scripts/pullDockerImages.sh
@@ -17,7 +17,7 @@ echo "Fetching images from Nexus"
 NEXUS_URL=nexus3.hyperledger.org:10001
 ORG_NAME="hyperledger/fabric"
 ARCH=$(go env GOARCH)
-LATEST_TAG=${LATEST_TAG:=$ARCH-1.4.0-stable}
+LATEST_TAG=${LATEST_TAG:=$ARCH-$BASE_VERSION-stable}
 echo "---------> REPO:" $REPO
 echo "---------> LATEST_TAG:" $LATEST_TAG
 
@@ -37,7 +37,7 @@ dockerTag() {
     docker tag $NEXUS_URL/$ORG_NAME-$IMAGE:$LATEST_TAG $ORG_NAME-$IMAGE
     docker tag $NEXUS_URL/$ORG_NAME-$IMAGE:$LATEST_TAG $ORG_NAME-$IMAGE:$LATEST_TAG
     if [ $IMAGE == javaenv ]; then
-        docker tag $NEXUS_URL/$ORG_NAME-$IMAGE:$LATEST_TAG $ORG_NAME-$IMAGE:$ARCH-1.4.0
+        docker tag $NEXUS_URL/$ORG_NAME-$IMAGE:$LATEST_TAG $ORG_NAME-$IMAGE:$ARCH-$BASE_VERSION
     fi
     echo "$ORG_NAME-$IMAGE:$LATEST_TAG"
     echo "Deleting Nexus docker images: $IMAGE"
@@ -49,12 +49,12 @@ dockerThirdParty() {
   for IMAGE in kafka zookeeper couchdb; do
     echo "$ORG_NAME-$IMAGE"
     # 3rd party images are on dockerhub
-    docker pull $ORG_NAME-$IMAGE:$ARCH-0.4.14
+    docker pull $ORG_NAME-$IMAGE:$ARCH-$BASEIMAGE_RELEASE
     if [ $? != 0 ]; then
        echo  "FAILED: Docker Pull Failed on $IMAGE"
        exit 1
     fi
-    docker tag $ORG_NAME-$IMAGE:$ARCH-0.4.14 $ORG_NAME-$IMAGE:latest
+    docker tag $ORG_NAME-$IMAGE:$ARCH-$BASEIMAGE_RELEASE $ORG_NAME-$IMAGE:latest
   done
 }
 
