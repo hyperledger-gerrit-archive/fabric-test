24b99d04845e19d5e9ffa55fa5392afac806c406
Dongming <dongming@ibm.com>
2019-04-17 16:35:05 -0400

    [FAB-14230] scritps to execute FAB-14230
    This CR includes scritps to execute
    FAB-14230, FAB-14269, FAB-14350, FAB-14922
    
    Change-Id: I1854fed8ebff350573b9e29ae2c91fcce57bee0d
    Signed-off-by: Dongming <dongming@ibm.com>

---
+++ tools/PTE/CITest/scenarios/FAB-14922.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Requirements:
+# The script assumes that a network with 3 org and 1 peer per org is operational, a channel is created and peers are joined, a chaincode
+# is installed and instantiated.  The corresponding PTE service credential json is placed in a directory under PTE.  The default directory
+# is PTEScaleTest-SC. If the user # chooses not to use the default directory, then he needs to change mySCDir below to the name of the
+# directory.
+
+# testcase: FAB-14922: RAFT scaling: increasingly large transaction
+# channels: 1
+# org: 1
+# thread: 1
+# tx: 1
+# traffic mode: Constant
+
+# source PTE CI utils
+source PTECIutils.sh
+
+myTESTCASE="FAB-14922"
+mySCDir="PTEScaleTest-SC"
+
+myCC="samplecc"
+myTXMODE="Constant"
+myNORG=1
+
+myNREQ=1
+
+chan0=1
+myMinChan=1
+myMaxChan=1
+myChanIncr=1
+myMinTh=1
+myMaxTh=1
+myThIncr=1
+myFreq=0
+
+myKey0=10000
+myKeyIncr=$myNREQ
+
+CWD=$PWD
+
+# remove existing PTE report
+CIpteReport=$LOGDir/$myTESTCASE"-pteReport.log"
+echo "CIpteReport=$CIpteReport"
+
+if [ -e $CIpteReport ]; then
+    rm -f $CIpteReport
+fi
+
+# execute PTE
+optString="--testcase $myTESTCASE --scdir $mySCDir -a $myCC --chan0 $chan0 --norg $myNORG --nreq $myNREQ --txmode $myTXMODE --freq $myFreq -i"
+echo "[$myTESTCASE] optString=$optString"
+PTEExecLoop $myMinChan $myMaxChan $myChanIncr $myMinTh $myMaxTh $myThIncr $myKey0 $myKeyIncr "${optString[@]}"
