64719c2392b80ee517111dfcd323a76c825ec60f
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-04-25 12:26:34 -0400

    [WIP] Testing the xml-parser
    Change-Id: I376ccedb9955ae2e8c7e70f0f03b0a9026b6aa19
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>

--- regression/smoke/runSmokeTestSuite.sh
+++ regression/smoke/runSmokeTestSuite.sh
@@ -12,20 +12,20 @@ cd ../../feature
 behave --junit --junit-directory ../regression/smoke/. --tags=-skip --tags=smoke -k -D logs=y
 cd -
 
-echo "======== Ledger component performance tests using LTE ========"
-py.test -v --junitxml results_ledger_lte_smoke.xml ledger_lte_smoke.py
+# echo "======== Ledger component performance tests using LTE ========"
+# py.test -v --junitxml results_ledger_lte_smoke.xml ledger_lte_smoke.py
 
-echo "======== Orderer component test using OTE and NL tools ========"
-py.test -v --junitxml results_orderer_ote.xml orderer_ote.py
+# echo "======== Orderer component test using OTE and NL tools ========"
+# py.test -v --junitxml results_orderer_ote.xml orderer_ote.py
 
-echo "======== Performance Test using PTE and NL tools ========"
-cd $GOPATH/src/github.com/hyperledger/fabric-test/tools/PTE
-npm config set prefix ~/npm
-npm install
-if [ $? != 0 ]; then
-    echo "FAILED: Failed to install npm. Cannot run pte test suite."
-    # Don't exit.. Continue with tests, to show the PTE failure results
-else
-    echo "Successfully installed npm."
-fi
+# echo "======== Performance Test using PTE and NL tools ========"
+# cd $GOPATH/src/github.com/hyperledger/fabric-test/tools/PTE
+# npm config set prefix ~/npm
+# npm install
+# if [ $? != 0 ]; then
+#     echo "FAILED: Failed to install npm. Cannot run pte test suite."
+#     # Don't exit.. Continue with tests, to show the PTE failure results
+# else
+#     echo "Successfully installed npm."
+# fi
 cd $SMOKEDIR && py.test -v --junitxml results_systest_pte.xml systest_pte.py
