ed66927a01cd043f483cccf1cb167ef596c0f300
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-05-22 02:15:33 -0400

    [WIP] Converting Network Laucher tool into GoLang
        Code for generating configtx.yaml
    
        Added code for generating crypto-config.yaml,
        reading the arguments from the command line,
        generating the crypto material, orderer genseis
        block and channel transaction
    
        Added K8s service template file and code for
        generating the K8s service yaml files for
        CA, Peer and Orderer
    
        Added template file and code for generating yaml
        file for ca, peer and orderer pods
    
        Modified crypto-config, k8s service and k8s pods
        template to use ytt
    
        Modified configtx template to use ytt and cleaned
        the networkLauncher.go file
    
        Need ytt-linux-amd64 binary in the NL/go folder
        to generate the configuration files
    
        Removed unused code from networkLauncher.go file
    
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: I89723b70324f1917bf49f35aa698388e80ec407f

---
+++ tools/NL/go/networkLauncher.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	yaml "gopkg.in/yaml.v2"
+)
+
+type NetworkLauncher struct {
+}
+
+// Config Struct
+type Config struct {
+	ArtifactsLocation    string                 `yaml:"certs_location,omitempty"`
+	NumChannels          int                    `yaml:"num_channels,omitempty"`
+}
+
+func (n *NetworkLauncher) getConf(networkSpecPath string) Config {
+	var config Config
+	yamlFile, err := ioutil.ReadFile(networkSpecPath)
+	if err != nil {
+		log.Fatalf("Failed to read input file %v", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		log.Fatalf("Failed to create config object %v", err)
+	}
+	return config
+}
+
+func (n *NetworkLauncher) GenerateConfigurationFiles() error {
+
+	cmd := exec.Command("./ytt-linux-amd64", "-f", "./templates/", "--output", "./configFiles")
+
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	fmt.Printf(string(stdoutStderr))
+	return nil
+}
+
+func (n *NetworkLauncher) GenerateCryptoCerts(networkSpec Config) error {
+
+	cmd := exec.Command("cryptogen", "generate", "--config=./configFiles/crypto-config.yaml", fmt.Sprintf("--output=%vcrypto-config", networkSpec.ArtifactsLocation))
+
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	fmt.Printf(string(stdoutStderr))
+	return nil
+}
+
+func (n *NetworkLauncher) GenerateOrdererGenesisBlock() error {
+	cmd := exec.Command("configtxgen", "-profile", "testOrgsOrdererGenesis", "-channelID", "ordersystemchannel", "-outputBlock", "./genesis.block")
+	stdoutStderr, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	fmt.Printf(string(stdoutStderr))
+	return nil
+}
+
+func (n *NetworkLauncher) GenerateChannelTransaction(networkSpec Config) error {
+
+	for i := 0; i < networkSpec.NumChannels; i++ {
+		cmd := exec.Command("configtxgen", "-profile", "testorgschannel", "-channelCreateTxBaseProfile", "testOrgsOrdererGenesis", "-channelID", fmt.Sprintf("testorgschannel%v", i), "-outputCreateChannelTx", fmt.Sprintf("./testorgschannel%v.tx", i))
+		stdoutStderr, err := cmd.CombinedOutput()
+		if err != nil {
+			return fmt.Errorf("%v", err)
+		}
+		fmt.Printf(string(stdoutStderr))
+	}
+	return nil
+}
+
+func (n *NetworkLauncher) ReadArguments() (*string, *string) {
+
+	networkSpecPath := flag.String("i", "", "Network spec input file path")
+	kubeConfigPath := flag.String("k", "", "Kube config file path")
+
+	flag.Parse()
+	if fmt.Sprintf("%s", *kubeConfigPath) == "" {
+		fmt.Println("Kube config file path not provided")
+	}
+	if fmt.Sprintf("%s", *networkSpecPath) == "" {
+		log.Fatalf("Network spec file path not provided")
+	}
+
+	return networkSpecPath, kubeConfigPath
+}
+
+func main() {
+	var NL NetworkLauncher
+
+	networkSpecPath, _ := NL.ReadArguments()
+	input := NL.getConf(*networkSpecPath)
+
+	err := NL.GenerateConfigurationFiles()
+	if err != nil {
+		log.Fatalf("Failed to generate yaml files%v", err)
+	}
+
+	err = NL.GenerateCryptoCerts(input)
+	if err != nil {
+		log.Fatalf("Failed to generate certificates %v", err)
+	}
+
+	err = NL.GenerateOrdererGenesisBlock()
+	if err != nil {
+		log.Fatalf("Failed to create orderer genesis block %v", err)
+	}
+
+	err = NL.GenerateChannelTransaction(input)
+	if err != nil {
+		log.Fatalf("Failed to create channel transaction %v", err)
+	}
+}
