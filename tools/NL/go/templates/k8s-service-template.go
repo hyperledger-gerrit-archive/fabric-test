049677ba9d9051a69795b29085d45d7081aaef3b
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-05-22 02:15:33 -0400

    [WIP] Converting Network Laucher tool into GoLang
        Code for generating configtx.yaml
    
        Added code for generating crypto-config.yaml,
        reading the arguments from the command line,
        generating the crypto material, orderer genseis
        block and channel transaction
    
        Added K8s service template file and code for
        generating the K8s service yaml files for
        CA, Peer and Orderer
    
        Added template file and code for generating yaml
        file for ca, peer and orderer pods
    
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: I89723b70324f1917bf49f35aa698388e80ec407f

---
+++ tools/NL/go/templates/k8s-service-template.go
@@ -0,0 +1,74 @@
+package templates
+
+func DefaultK8sServiceTemplate() string {
+	const template = `{{ range .PeerOrganizations }}
+{{ $w := .}}
+{{ range $i, $_ := N $w.NumCa }}
+---
+kind: Service
+appVersion: v1
+metadata:
+  labels:
+    k8s-app: ca{{ $i }}-{{ $w.Name }}
+  name: ca{{ $i }}-{{ $w.Name }}
+spec:
+  selector:
+    k8s-app: ca{{ $i }}-{{ $w.Name }}
+  ports:
+    - name: port1
+      port: 7054 
+{{- end }}
+{{ range $i, $_ := N $w.NumPeers }}
+---
+kind: Service
+appVersion: v1
+metadata:
+  labels:
+    k8s-app: peer{{ $i }}-{{ $w.Name }}
+  name: peer{{ $i }}-{{ $w.Name }}
+spec:
+  selector:
+    k8s-app: peer{{ $i }}-{{ $w.Name }}
+  ports:
+    - name: port1
+      port: 7051
+    - name: port2
+      port: 7052 
+{{- end }}
+{{- end }}
+
+{{ range .OrdererOrganizations }}
+{{ $w := .}}
+{{ range $i, $_ := N $w.NumCa }}
+---
+kind: Service
+appVersion: v1
+metadata:
+  labels:
+    k8s-app: ca{{ $i }}-{{ $w.Name }}
+  name: ca{{ $i }}-{{ $w.Name }}
+spec:
+  selector:
+    k8s-app: ca{{ $i }}-{{ $w.Name }}
+  ports:
+    - name: port1
+      port: 7054 
+{{- end }}
+{{ range $i, $_ := N $w.NumOrderers }}
+---
+kind: Service
+appVersion: v1
+metadata:
+  labels:
+    k8s-app: orderer{{ $i }}-{{ $w.Name }}
+  name: orderer{{ $i }}-{{ $w.Name }}
+spec:
+  selector:
+    k8s-app: orderer{{ $i }}-{{ $w.Name }}
+  ports:
+    - name: port1
+      port: 7050
+{{- end }}
+{{- end }}`
+	return template
+}
