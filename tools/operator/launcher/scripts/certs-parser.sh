1c3d3a62531a530e32d2f11eda98807101f1cf7b
Surya Lanka <suryalnvs@gmail.com>
2019-08-12 09:27:32 -0400

    [FAB-16248] Fixing launcher when using fabric alpine
    Fixing launcher to use sh instead of bash when using
    fabric 2.0 images
    
    Signed-off-by: Surya Lanka <suryalnvs@gmail.com>
    Change-Id: I2e0beb4cab2158a75f1a80999e5911da66690b9a

--- tools/operator/launcher/scripts/certs-parser.sh
+++ tools/operator/launcher/scripts/certs-parser.sh
@@ -1,12 +1,13 @@
+#!/bin/sh
 # Copyright IBM Corp. All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
-#!/bin/bash
-if [ $(which apt | wc -l) == '1' ]
+
+if [ "$(which apt | wc -l)" = 1 ]
 then
   apt-get update
   apt-get install -y jq
-elif [ $(which apk | wc -l) == '1' ]
+elif [ "$(which apk | wc -l)" = 1 ]
 then
   apk add jq
 fi
@@ -18,53 +19,53 @@ set -e
 echo "#####Creating MSP directory structure#####"
 mkdir -p $MSPDIR/msp/admincerts $MSPDIR/msp/cacerts $MSPDIR/msp/signcerts $MSPDIR/msp/tlscacerts $MSPDIR/msp/keystore $MSPDIR/tls $MSPDIR/ca $MSPDIR/tlsca
 
-ADMINCERT=$(eval echo $(cat $MSPSECRET | jq '.msp.admin_pem'))
-echo -e $ADMINCERT > $MSPDIR/msp/admincerts/cert.pem
-truncate -s -1 $MSPDIR/msp/admincerts/cert.pem
+ADMINCERT=$(eval echo "$(cat "$MSPSECRET" | jq '.msp.admin_pem')")
+echo "$ADMINCERT" > $MSPDIR/msp/admincerts/cert.pem
+sed -i '/^\s*$/d' $MSPDIR/msp/admincerts/cert.pem
 
-CACERT=$(eval echo $(cat $MSPSECRET | jq '.msp.ca_pem'))
-echo -e $CACERT > $MSPDIR/msp/cacerts/ca-cert.pem
-truncate -s -1 $MSPDIR/msp/cacerts/ca-cert.pem
+CACERT=$(eval echo "$(cat "$MSPSECRET" | jq '.msp.ca_pem')")
+echo "$CACERT" > $MSPDIR/msp/cacerts/ca-cert.pem
+sed -i '/^\s*$/d' $MSPDIR/msp/cacerts/ca-cert.pem
 
-SIGNCERT=$(eval echo $(cat $MSPSECRET | jq '.msp.pem'))
-echo -e $SIGNCERT > $MSPDIR/msp/signcerts/cert.pem
-truncate -s -1 $MSPDIR/msp/signcerts/cert.pem
+SIGNCERT=$(eval echo "$(cat "$MSPSECRET" | jq '.msp.pem')")
+echo "$SIGNCERT" > $MSPDIR/msp/signcerts/cert.pem
+sed -i '/^\s*$/d' $MSPDIR/msp/signcerts/cert.pem
 
-TLSCACERT=$(eval echo $(cat $MSPSECRET | jq '.msp.tls_pem'))
-echo -e $TLSCACERT > $MSPDIR/msp/tlscacerts/tlsca-cert.pem
-truncate -s -1 $MSPDIR/msp/tlscacerts/tlsca-cert.pem
+TLSCACERT=$(eval echo "$(cat "$MSPSECRET" | jq '.msp.tls_pem')")
+echo "$TLSCACERT" > $MSPDIR/msp/tlscacerts/tlsca-cert.pem
+sed -i '/^\s*$/d' $MSPDIR/msp/tlscacerts/tlsca-cert.pem
 
-PRIVKEY=$(eval echo $(cat $MSPSECRET | jq '.msp.private_key'))
-echo -e $PRIVKEY > $MSPDIR/msp/keystore/cert.key
-truncate -s -1 $MSPDIR/msp/keystore/cert.key
+PRIVKEY=$(eval echo "$(cat "$MSPSECRET" | jq '.msp.private_key')")
+echo "$PRIVKEY" > $MSPDIR/msp/keystore/cert.key
+sed -i '/^\s*$/d' $MSPDIR/msp/keystore/cert.key
 
-TLS_CA_CRT=$(eval echo $(cat $MSPSECRET | jq '.tls.ca_cert'))
-echo -e $TLS_CA_CRT > $MSPDIR/tls/ca.crt
-truncate -s -1 $MSPDIR/tls/ca.crt
+TLS_CA_CRT=$(eval echo "$(cat "$MSPSECRET" | jq '.tls.ca_cert')")
+echo "$TLS_CA_CRT" > $MSPDIR/tls/ca.crt
+sed -i '/^\s*$/d' $MSPDIR/tls/ca.crt
 
-TLS_SERVER_CRT=$(eval echo $(cat $MSPSECRET | jq '.tls.server_cert'))
-echo -e $TLS_SERVER_CRT > $MSPDIR/tls/server.crt
-truncate -s -1 $MSPDIR/tls/server.crt
+TLS_SERVER_CRT=$(eval echo "$(cat "$MSPSECRET" | jq '.tls.server_cert')")
+echo "$TLS_SERVER_CRT" > $MSPDIR/tls/server.crt
+sed -i '/^\s*$/d' $MSPDIR/tls/server.crt
 
-TLS_SERVER_KEY=$(eval echo $(cat $MSPSECRET | jq '.tls.server_key'))
-echo -e $TLS_SERVER_KEY > $MSPDIR/tls/server.key
-truncate -s -1 $MSPDIR/tls/server.key
+TLS_SERVER_KEY=$(eval echo "$(cat "$MSPSECRET" | jq '.tls.server_key')")
+echo "$TLS_SERVER_KEY" > $MSPDIR/tls/server.key
+sed -i '/^\s*$/d' $MSPDIR/tls/server.key
 
-CA_CRT=$(eval echo $(cat $MSPSECRET | jq '.ca.pem'))
-echo -e $CA_CRT > $MSPDIR/ca/ca-cert.pem
-truncate -s -1 $MSPDIR/ca/ca-cert.pem
+CA_CRT=$(eval echo "$(cat "$MSPSECRET" | jq '.ca.pem')")
+echo "$CA_CRT" > $MSPDIR/ca/ca-cert.pem
+sed -i '/^\s*$/d' $MSPDIR/ca/ca-cert.pem
 
-CA_PRIVATE_KEY=$(eval echo $(cat $MSPSECRET | jq '.ca.private_key'))
-echo -e $CA_PRIVATE_KEY > $MSPDIR/ca/ca_private.key
-truncate -s -1 $MSPDIR/ca/ca_private.key
+CA_PRIVATE_KEY=$(eval echo "$(cat "$MSPSECRET" | jq '.ca.private_key')")
+echo "$CA_PRIVATE_KEY" > $MSPDIR/ca/ca_private.key
+sed -i '/^\s*$/d' $MSPDIR/ca/ca_private.key
 
-TLSCA_CRT=$(eval echo $(cat $MSPSECRET | jq '.tlsca.pem'))
-echo -e $TLSCA_CRT > $MSPDIR/tlsca/tlsca-cert.pem
-truncate -s -1 $MSPDIR/tlsca/tlsca-cert.pem
+TLSCA_CRT=$(eval echo "$(cat "$MSPSECRET" | jq '.tlsca.pem')")
+echo "$TLSCA_CRT" > $MSPDIR/tlsca/tlsca-cert.pem
+sed -i '/^\s*$/d' $MSPDIR/tlsca/tlsca-cert.pem
 
-TLSCA_PRIVATE_KEY=$(eval echo $(cat $MSPSECRET | jq '.tlsca.private_key'))
-echo -e $TLSCA_PRIVATE_KEY > $MSPDIR/tlsca/tlsca_private.key
-truncate -s -1 $MSPDIR/tlsca/tlsca_private.key
+TLSCA_PRIVATE_KEY=$(eval echo "$(cat "$MSPSECRET" | jq '.tlsca.private_key')")
+echo "$TLSCA_PRIVATE_KEY" > $MSPDIR/tlsca/tlsca_private.key
+sed -i '/^\s*$/d' $MSPDIR/tlsca/tlsca_private.key
 
 echo "#####Completed creating MSP directory structure#####"
 
