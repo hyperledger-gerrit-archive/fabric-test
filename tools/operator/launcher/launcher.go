56f56fde5c277b44ce468b50aeedf478bb52fda5
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-08-12 14:41:02 -0400

    [FAB-16275] Modify operator to run k8s commands
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: I7512249553216d40380b472e4d1e03199ed63195
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>

--- tools/operator/launcher/launcher.go
+++ tools/operator/launcher/launcher.go
@@ -45,7 +45,7 @@ func doAction(action string, input networkspec.Config, kubeConfigPath string) {
 		}
 
 		if kubeConfigPath != "" {
-			err = nl.CreateMspSecret(input, kubeConfigPath)
+			err = nl.Msp(input, kubeConfigPath)
 			if err != nil {
 				log.Fatal(err)
 			}
