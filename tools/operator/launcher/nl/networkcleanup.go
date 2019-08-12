d3b9a6c1654bfc4c5fba0dd85961e3df0f0f81bd
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-08-12 14:41:02 -0400

    [FAB-16275] Modify operator to run k8s commands
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: I7512249553216d40380b472e4d1e03199ed63195
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>

--- tools/operator/launcher/nl/networkcleanup.go
+++ tools/operator/launcher/nl/networkcleanup.go
@@ -6,7 +6,6 @@ package nl
 
 import (
 	"os"
-	"github.com/hyperledger/fabric-test/tools/operator/client"
 	"github.com/hyperledger/fabric-test/tools/operator/utils"
 	"github.com/hyperledger/fabric-test/tools/operator/networkspec"
 )
@@ -28,9 +27,6 @@ func NetworkCleanUp(input networkspec.Config, kubeConfigPath string) error {
 	err = os.RemoveAll(utils.ChannelArtifactsDir(artifactsLocation))
 	err = os.RemoveAll(utils.CryptoConfigDir(artifactsLocation))
 	err = os.RemoveAll(utils.ConnectionProfilesDir(artifactsLocation))
-	if input.K8s.DataPersistence == "local" && kubeConfigPath != "" {
-		err = client.ExecuteK8sCommand(kubeConfigPath, true, "delete", "-f", "./scripts/alpine.yaml")
-	}
 	if err != nil {
 		return err
 	}
