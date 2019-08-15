ab33db8f46c8ba1666ec3b783ad9d00e26290129
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-08-14 23:27:51 -0400

    [FAB-16311] Restructuring k8s and docker in operator
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: I20143c6b5a22daa599893d912793669ef4c67798

---
+++ tools/operator/launcher/k8s/connprofile.go
@@ -0,0 +1,217 @@
+package k8s
+
+import (
+	"fmt"
+	"strings"
+	"reflect"
+
+	"github.com/hyperledger/fabric-test/tools/operator/logger"
+	"github.com/hyperledger/fabric-test/tools/operator/client"
+	"github.com/hyperledger/fabric-test/tools/operator/connectionprofile"
+	"github.com/hyperledger/fabric-test/tools/operator/networkspec"
+	"github.com/hyperledger/fabric-test/tools/operator/utils"
+	yaml "gopkg.in/yaml.v2"
+)
+
+//K8sExternalIP -- To get the externalIP of a fabric component
+func (k K8s) K8sExternalIP(input networkspec.Config, serviceName string) (string, error) {
+
+	var nodeIP string
+	var inputArgs []string
+	if input.K8s.ServiceType == "NodePort" {
+		inputArgs = []string{"get", "nodes", "-o", `jsonpath='{ $.items[*].status.addresses[?(@.type=="ExternalIP")].address }'`}
+		k.Arguments = inputArgs
+		output, err := client.ExecuteK8sCommand(k.Args(), false)
+		if err != nil {
+			logger.INFO("Failed to get the external IP for k8s using NodePor")
+			return "", err
+		}
+		nodeIPList := strings.Split(string(output)[1:], " ")
+		nodeIP = nodeIPList[0]
+	} else if input.K8s.ServiceType == "LoadBalancer" {
+		inputArgs = []string{"get", "-o", `jsonpath="{.status.loadBalancer.ingress[0].ip}"`, "services", serviceName}
+		k.Arguments = inputArgs
+		output, err := client.ExecuteK8sCommand(k.Args(), false)
+		if err != nil {
+			logger.INFO("Failed to get the external IP for k8s using NodePort")
+			return "", err
+		}
+		nodeIP = string(output)[1 : len(string(output))-1]
+	}
+
+	return nodeIP, nil
+}
+
+//K8sServicePort -- To get the port number of a fabric k8s component
+func (k K8s) K8sServicePort(serviceName, serviceType string, forHealth bool) (string, error) {
+
+	var k8Ports K8Ports
+	index := 0
+	if forHealth {
+		index = 1
+	}
+	args := []string{"get", "-o", "json", "services", serviceName}
+	k.Arguments = args
+	output, err := client.ExecuteK8sCommand(k.Args(), false)
+	if err != nil {
+		logger.INFO("Failed to get the port number for service ", serviceName)
+		return "", err
+	}
+	err = yaml.Unmarshal([]byte(output), &k8Ports)
+	if err != nil {
+		logger.INFO("Failed to unmarshall k8sPort object")
+	}
+	if serviceType == "LoadBalancer" {
+		if forHealth{
+			return k8Ports.Spec.Ports[index].NodePort, nil	
+		}
+		return k8Ports.Spec.Ports[0].TargetPort, nil
+	}
+	return k8Ports.Spec.Ports[index].NodePort, nil
+}
+
+func (k K8s) ordererOrganizations(input networkspec.Config) (map[string]networkspec.Orderer, error) {
+
+	orderers := make(map[string]networkspec.Orderer)
+	artifactsLocation := input.ArtifactsLocation
+	ordererOrgsPath := utils.OrdererOrgsDir(artifactsLocation)
+	var err error
+	var orderer networkspec.Orderer
+	var portNumber, nodeIP string
+	protocol := "grpc"
+	if input.TLS == "true" || input.TLS == "mutual" {
+		protocol = "grpcs"
+	}
+	for org := 0; org < len(input.OrdererOrganizations); org++ {
+		ordererOrg := input.OrdererOrganizations[org]
+		orgName := ordererOrg.Name
+		for i := 0; i < ordererOrg.NumOrderers; i++ {
+			ordererName := fmt.Sprintf("orderer%d-%s", i, orgName)
+			portNumber, err = k.K8sServicePort(ordererName, input.K8s.ServiceType, false)
+			if err != nil {
+				return orderers, err
+			}
+			nodeIP, err = k.K8sExternalIP(input, ordererName)
+			if err != nil {
+				return orderers, err
+			}
+			orderer = networkspec.Orderer{MSPID: ordererOrg.MSPID, URL: fmt.Sprintf("%s://%s:%s", protocol, nodeIP, portNumber), AdminPath: utils.JoinPath(ordererOrgsPath, fmt.Sprintf("%s/users/Admin@%s/msp", orgName, orgName))}
+			orderer.GrpcOptions.SslTarget = ordererName
+			orderer.TLSCACerts.Path = utils.JoinPath(ordererOrgsPath, fmt.Sprintf("%s/orderers/%s.%s/msp/tlscacerts/tlsca.%s-cert.pem", orgName, ordererName, orgName, orgName))
+			orderers[ordererName] = orderer
+		}
+	}
+	return orderers, nil
+}
+
+func (k K8s) certificateAuthorities(peerOrg networkspec.PeerOrganizations, input networkspec.Config) (map[string]networkspec.CertificateAuthority, error) {
+
+	CAs := make(map[string]networkspec.CertificateAuthority)
+	var err error
+	var CA networkspec.CertificateAuthority
+	var portNumber, nodeIP string
+	protocol := "http"
+	if input.TLS == "true" || input.TLS == "mutual" {
+		protocol = "https"
+	}
+	artifactsLocation := input.ArtifactsLocation
+	orgName := peerOrg.Name
+	for i := 0; i < peerOrg.NumCA; i++ {
+		caName := fmt.Sprintf("ca%d-%s", i, orgName)
+		portNumber, err = k.K8sServicePort(caName, input.K8s.ServiceType, false)
+		if err != nil {
+			return CAs, err
+		}
+		nodeIP, err = k.K8sExternalIP(input, caName)
+		if err != nil {
+			return CAs, err
+		}
+		CA = networkspec.CertificateAuthority{URL: fmt.Sprintf("%s://%s:%s", protocol, nodeIP, portNumber), CAName: caName}
+		CA.TLSCACerts.Path = utils.JoinPath(utils.PeerOrgsDir(artifactsLocation), fmt.Sprintf("%s/ca/ca.%s-cert.pem", orgName, orgName))
+		CA.HTTPOptions.Verify = false
+		CA.Registrar.EnrollID, CA.Registrar.EnrollSecret = "admin", "adminpw"
+		CAs[fmt.Sprintf("ca%d", i)] = CA
+	}
+	return CAs, nil
+}
+
+func getKeysFromMap(newMap interface{}) []string {
+
+	var componentsList []string
+	v := reflect.ValueOf(newMap)
+	if v.Kind() != reflect.Map {
+		logger.INFO("not a map!")
+		return nil
+	}
+	keys := v.MapKeys()
+	for i := range keys {
+		componentsList = append(componentsList, fmt.Sprintf("%s", keys[i]))
+	}
+	return componentsList
+}
+
+func (k K8s) peersPerOrganization(peerorg networkspec.PeerOrganizations, input networkspec.Config) (map[string]networkspec.Peer, error) {
+
+	var err error
+	var peer networkspec.Peer
+	var portNumber, nodeIP string
+	peers := make(map[string]networkspec.Peer)
+	protocol := "grpc"
+	peerOrgsLocation := utils.PeerOrgsDir(input.ArtifactsLocation)
+	if input.TLS == "true" || input.TLS == "mutual" {
+		protocol = "grpcs"
+	}
+	for i := 0; i < peerorg.NumPeers; i++ {
+		peerName := fmt.Sprintf("peer%d-%s", i, peerorg.Name)
+		portNumber, err = k.K8sServicePort(peerName, input.K8s.ServiceType, false)
+		if err != nil {
+			return peers, err
+		}
+		nodeIP, err = k.K8sExternalIP(input, peerName)
+		if err != nil {
+			return peers, err
+		}
+		peer = networkspec.Peer{URL: fmt.Sprintf("%s://%s:%s", protocol, nodeIP, portNumber)}
+		peer.GrpcOptions.SslTarget = peerName
+		peer.TLSCACerts.Path = utils.JoinPath(peerOrgsLocation, fmt.Sprintf("%s/tlsca/tlsca.%s-cert.pem", peerorg.Name, peerorg.Name))
+		peers[peerName] = peer
+	}
+	return peers, nil
+}
+
+//GenerateConnectionProfiles -- To generate conenction profiles
+func (k K8s) GenerateConnectionProfiles(input networkspec.Config) error {
+
+	orderersMap, err := k.ordererOrganizations(input)
+	if err != nil {
+		return err
+	}
+	connProfile := connectionprofile.ConnProfile{Orderers: orderersMap, Input: input}
+	for org := 0; org < len(input.PeerOrganizations); org++ {
+		organizations := make(map[string]networkspec.Organization)
+		peerorg := input.PeerOrganizations[org]
+		peersMap, err := k.peersPerOrganization(peerorg, input)
+		if err != nil {
+			return err
+		}
+		connProfile.Peers = peersMap
+		ca, err := k.certificateAuthorities(peerorg, input)
+		if err != nil {
+			return err
+		}
+		connProfile.CA = ca
+		caList := make([]string, 0, len(ca))
+		for k := range ca {
+			caList = append(caList, k)
+		}
+		org := connProfile.Organization(peerorg,  caList)
+		organizations[peerorg.Name] = org
+		connProfile.Organizations = organizations
+		err = connProfile.GenerateConnProfilePerOrg(peerorg.Name)
+		if err != nil {
+			logger.INFO("Failed to generate connection profile")
+			return err
+		}
+	}
+	return nil
+}
--- tools/operator/logger/logger.go
+++ tools/operator/logger/logger.go
@@ -8,6 +8,7 @@ import (
 
 //INFO -- To print the info logs
 func INFO(message ...string) {
+
 	info := log.New(os.Stdout,
 		"INFO: ",
 		log.Ldate|log.Ltime)
@@ -16,11 +17,12 @@ func INFO(message ...string) {
 
 //ERROR -- To print the error logs
 func ERROR(err error, message ...string) {
+
 	error := log.New(os.Stderr,
 		"ERROR: ",
 		log.Ldate|log.Ltime)
 	if err == nil {
 		error.Fatalln(strings.Join(message, ""))
 	}
-	error.Fatalf("%s; err: %s", strings.Join(message, ""), err)
+	error.Fatalf("%s", strings.Join(message, ""), err)
 }
