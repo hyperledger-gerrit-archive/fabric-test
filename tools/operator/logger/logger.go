534638f8b880dffd342bf96f9f5be41808826ddc
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-08-15 16:01:26 -0400

    [FAB-16312] Remove PrintLogs and FatalLogs in operator
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: I5035416fbb0c0402473bd6db79ff5e2599957a8e

---
+++ tools/operator/logger/logger.go
@@ -0,0 +1,26 @@
+package logger
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+//INFO -- To print the info logs
+func INFO(message ...string) {
+	info := log.New(os.Stdout,
+		"INFO: ",
+		log.Ldate|log.Ltime)
+	info.Println(strings.Join(message, ""))
+}
+
+//ERROR -- To print the error logs
+func ERROR(err error, message ...string) {
+	error := log.New(os.Stderr,
+		"ERROR: ",
+		log.Ldate|log.Ltime)
+	if err == nil {
+		error.Fatalln(strings.Join(message, ""))
+	}
+	error.Fatalf("%s; err: %s", strings.Join(message, ""), err)
+}
