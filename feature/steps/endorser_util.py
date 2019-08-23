7551490c252eef8db83077637d95525e4588f476
Scott Zwierzynski <scottz@us.ibm.com>
2019-08-23 13:57:09 -0400

    [FAB-15471] fix 1.4 behave tests
    1. Use sh instead of bash, because
       supported on alpine.
    
    2. Bring back the common and peer protos
       that were removed recently from the
       feature-upgrade/ directory that are
       needed for tests in feature/ dir.
    
    3. fix the creation of the genesis block file
    
    Change-Id: I49b3e418183a19c7f49e9380232d3166d54b6fd1
    Signed-off-by: Scott Zwierzynski <scottz@us.ibm.com>

--- feature/steps/endorser_util.py
+++ feature/steps/endorser_util.py
@@ -17,11 +17,11 @@ import time
 import common_util
 
 try:
-    pbFilePath = "../feature-upgrade"
+    pbFilePath = "../feature"
     sys.path.insert(0, pbFilePath)
     from peer import chaincode_pb2
 except:
-    print("ERROR! Unable to import the protobuf libraries from the ../feature-upgrade directory: {0}".format(sys.exc_info()[0]))
+    print("ERROR! Failed to import the protobuf libraries chaincode_pb2 from the ../feature/peer/ directory: {0}".format(sys.exc_info()[0]))
     sys.exit(1)
 
 # The default channel ID
