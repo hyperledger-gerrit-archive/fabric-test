9268d9d0a25024c28f5293e1fcb6cf68cdac3031
rameshthoomu <rameshbabu.thoomu@gmail.com>
2019-08-27 10:51:05 -0400

    FAB-16070 Enable 12hr longrun test in k8s
    This patch consists of configuration and networkspec files to test
    12hr longrun test in k8s environment. Added a new target
    svt-weekly-pte-12hr-test-k8s in makefile. Before execute this target
    make sure you have k8s cluster available, kubeconfig file downloaded in
    PTE client machine.
    
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>
    Change-Id: Ie64362efe1b993f2a71380d52c6eef6275c4fa76

---
+++ regression/weekly/12HrTest_k8s.py
@@ -0,0 +1,84 @@
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import unittest
+import subprocess
+
+logs_directory = '../../tools/PTE/CITest/scripts'
+operator_directory = '../../tools/operator'
+k8s_testsuite = '../../tools/PTE/CITest/k8s_testsuite/scripts'
+
+# error messages
+testScriptFailed =      "Test Failed with non-zero exit code; check for errors in fabric-test/tools/PTE/CITest"
+invokeFailure =         "Error: incorrect number of INVOKE transactions sent or received"
+queryCountFailure =     "Error: incorrect number of QUERY transactions sent or received"
+
+
+class System_Tests_Kafka_Couchdb_TLS(unittest.TestCase):
+
+
+    def test_1downNetwork(self):
+        '''
+        Description:
+
+        '''
+
+        # Teardown the network
+        returncode = subprocess.call("./operator.sh -a down -f kafka_couchdb_tls.yaml", cwd=k8s_testsuite, shell=True)
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
+
+
+    def test_2launchNetwork(self):
+        '''
+        Description:
+
+        '''
+
+        # Launch the network
+        returncode = subprocess.call("./operator.sh -a up -f kafka_couchdb_tls.yaml", cwd=k8s_testsuite, shell=True)
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
+
+    def test_3createJoinChannel(self):
+        '''
+        Description:
+
+        '''
+
+        returncode = subprocess.call("./operator.sh -c", cwd=k8s_testsuite, shell=True)
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
+
+    def test_4installInstantiation(self):
+        '''
+        Description:
+
+        '''
+
+        returncode = subprocess.call("./operator.sh -i", cwd=k8s_testsuite, shell=True)
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
+
+    def test_5samplecc_orgAnchor_2chan(self):
+        '''
+        Description:
+
+        '''
+
+        # Run the test scenario: Execute invokes and query tests.
+        returncode = subprocess.call("./operator.sh -t samplecc_go_12hr", cwd=k8s_testsuite, shell=True)
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
+
+        allThreadsCompleted = subprocess.check_output(
+                "grep \"pte-main:completed:\" samplecc_go_12hr_i.log | wc -l",
+                cwd=logs_directory, shell=True)
+        self.assertEqual(int(allThreadsCompleted.strip()), 2)
+
+    def test_6tearDownNetwork(self):
+        '''
+        Description:
+
+        '''
+
+        # Teardown the network
+        returncode = subprocess.call("./operator.sh -a down -f kafka_couchdb_tls.yaml", cwd=k8s_testsuite, shell=True)
+        self.assertEqual(returncode, 0, msg=testScriptFailed)
