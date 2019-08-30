6d9b069d1029fc41e08fdb9d48ca969e8f1a0e69
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-08-30 11:09:04 -0400

    [FAB-16081] Create objects for install cc for pte
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: I29dafffedc15d57547fae4baa6d23fda4b0f589d

--- tools/operator/testclient/helper/structs.go
+++ tools/operator/testclient/helper/structs.go
@@ -8,6 +8,7 @@ type Config struct {
 	CreateChannel     []Channel          `yaml:"createChannel,omitempty"`
 	AnchorPeerUpdate  []AnchorPeerUpdate `yaml:"anchorPeerUpdate,omitempty"`
 	JoinChannel       []Channel          `yaml:"joinChannel,omitempty"`
+	InstallCC         []InstallCC        `yaml:"installChaincode,omitempty"`
 }
 
 //Channel --
@@ -34,73 +35,12 @@ type Organization struct {
 	ConnProfilePath string `yaml:"connProfilePath,omitempty"`
 }
 
-//ChainCodeObject --
-type ChainCodeObject struct {
-	ChainCodeID      string `json:"chaincodeID,omitempty"`
-	ChainCodeVersion string `json:"chaincodeVer,omitempty"`
-	Deploy           struct {
-		ChainCodePath string   `json:"chaincodePath,omitempty"`
-		MetadataPath  string   `json:"metadataPath,omitempty"`
-		Language      string   `json:"language,omitempty"`
-		Fcn           string   `json:"fcn,omitempty"`
-		Args          []string `json:"args,omitempty"`
-	} `json:"deploy,omitempty"`
-	*Channel
-}
-
-//InvokeTranscations --
-type InvokeTranscations struct {
-	LogLevel        string `json:"logLevel,omitempty"`
-	InvokeCheck     string `json:"invokeCheck,omitempty"`
-	TransMode       string `json:"transMode,omitempty"`
-	TransType       string `json:"transType,omitempty"`
-	InvokeType      string `json:"invokeType,omitempty"`
-	TargetPeers     string `json:"targetPeers,omitempty"`
-	PeerFailover    string `json:"peerFailover,omitempty"`
-	OrdererFailover string `json:"ordererFailover,omitempty"`
-
-	NProcPerOrg string `json:"nProcPerOrg,omitempty"`
-	NRequest    string `json:"nRequest,omitempty"`
-	RunDur      string `json:"runDur,omitempty"`
-	BurstOpt    struct {
-		BurstFreq []string `json:"burstFreq,omitempty"`
-		BurstDur  []string `json:"burstDur,omitempty"`
-	} `json:"burstOpt,omitempty"`
-	MixOpt struct {
-		MixQuery string `json:"mixQuery,omitempty"`
-		MixFreq  string `json:"mixFreq,omitempty"`
-	} `json:"mixOpt,omitempty"`
-	ConstantOpt struct {
-		RecHist   string `json:"recHist,omitempty"`
-		ConstFreq string `json:"constFreq,omitempty"`
-		DevFreq   string `json:"devFreq,omitempty"`
-	} `json:"constantOpt,omitempty"`
-	ListOpt struct {
-		Org1 []string `json:"org1,omitempty"`
-		Org2 []string `json:"org2,omitempty"`
-	} `json:"listOpt,omitempty"`
-	EventOpt struct {
-		Type     string `json:"type,omitempty"`
-		Listener string `json:"listener,omitempty"`
-		Timeout  string `json:"timeout,omitempty"`
-	} `json:"eventOpt,omitempty"`
-	FailoverOpt struct {
-		Method string `json:"method,omitempty"`
-		List   string `json:"list,omitempty"`
-	} `json:"failoverOpt,omitempty"`
-	InvokeCheckOpt struct {
-		Peers        string `json:"peers,omitempty"`
-		Transactions string `json:"transactions,omitempty"`
-		TxNum        string `json:"txNum,omitempty"`
-	} `json:"invokeCheckOpt,omitempty"`
-	OrdererOpt struct {
-		Method    string `json:"method,omitempty"`
-		NOrderers string `json:"nOrderers,omitempty"`
-	} `json:"ordererOpt,omitempty"`
-	TimeoutOpt struct {
-		PreConfig   string `json:"preConfig,omitempty"`
-		Request     string `json:"request,omitempty"`
-		GrpcTimeout string `json:"grpcTimeout,omitempty"`
-	} `json:"timeoutOpt,omitempty"`
-	*ChainCodeObject
+//InstallCC --
+type InstallCC struct {
+	ChainCodeName    string `yaml:"chaincodeID,omitempty"`
+	ChainCodeVersion string `yaml:"chaincodeVer,omitempty"`
+	ChainCodePath    string `yaml:"chaincodePath,omitempty"`
+	Organizations    string `yaml:"organizations,omitempty"`
+	Language         string `yaml:"language,omitempty"`
+	MetadataPath     string `yaml:"metadataPath,omitempty"`
 }
