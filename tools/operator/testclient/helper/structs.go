3fa60f23a30b861b27baec2b25c3da07ecbd22ad
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-09-03 01:38:37 -0400

    [FAB-16082] Create instantiate cc object for pte
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: I3730508ed2b71c7b81c6580d1730015b5fe821e6

--- tools/operator/testclient/helper/structs.go
+++ tools/operator/testclient/helper/structs.go
@@ -48,15 +48,59 @@ type InstallCC struct {
 
 //InstantiateCC --
 type InstantiateCC struct {
+	ChannelName       string `yaml:"channelName,omitempty"`
 	ChainCodeName     string `yaml:"chaincodeName,omitempty"`
 	ChainCodeVersion  string `yaml:"ccVersion,omitempty"`
-	ChainCodePath     string `yaml:"chaincodePath,omitempty"`
 	Organizations     string `yaml:"organizations,omitempty"`
+	Arguments         string `yaml:"args,omitempty"`
 	EndorsementPolicy string `yaml:"endorsementPolicy,omitempty"`
+	ChannelPrefix     string `yaml:"channelPrefix,omitempty"`
+	NumChannels       int    `yaml:"numChannels,omitempty"`
 	CollectionPath    string `yaml:"collectionPath,omitempty"`
-	Language          string `yaml:"language,omitempty"`
 	TimeOutOpt        struct {
 		PreConfig string `yaml:"preConfig,omitempty"`
 		Request   string `yaml:"request,omitempty"`
 	} `yaml:"timeoutOpt,omitempty"`
+}
+
+//Invoke --
+type Invoke struct {
+	ChannelName   string `yaml:"channelName,omitempty"`
+	TargetPeers   string `yaml:"targetPeers,omitempty"`
+	NProcPerOrg   int    `yaml:"nProcPerOrg,omitempty"`
+	NRequest      int    `yaml:"nRequest,omitempty"`
+	RunDuration   int    `yaml:"runDur,omitempty"`
+	Organizations string `yaml:"organizations,omitempty"`
+	TxnOptions    []struct {
+		Mode    string `yaml:"mode,omitempty"`
+		Options string `yaml:"options,omitempty"`
+	} `yaml:"txnOpts,omitempty"`
+	QueryCheck   string `yaml:"queryCheck,omitempty"`
+	EventOptions struct {
+		Type     string `yaml:"type,omitempty"`
+		Listener string `yaml:"listener,omitempty"`
+		Timeout  string `yaml:"timeout,omitempty"`
+	} `yaml:"eventOpt,omitempty"`
+	CCOptions struct {
+		CCType     string `yaml:"ccType,omitempty"`
+		KeyStart   int    `yaml:"keyStart,omitempty"`
+		PayLoadMin string `yaml:"payLoadMin,omitempty"`
+		PayLoadMax string `yaml:"payLoadMax,omitempty"`
+	} `yaml:"ccOpt,omitempty"`
+	MoveArgs string `yaml:"moveArgs,omitempty"`
+}
+
+//Query --
+type Query struct {
+	ChannelName   string `yaml:"channelName,omitempty"`
+	TargetPeers   string `yaml:"targetPeers,omitempty"`
+	NProcPerOrg   int    `yaml:"nProcPerOrg,omitempty"`
+	NRequest      int    `yaml:"nRequest,omitempty"`
+	RunDuration   int    `yaml:"runDur,omitempty"`
+	Organizations string `yaml:"organizations,omitempty"`
+	CCOptions     struct {
+		CCType   string `yaml:"ccType,omitempty"`
+		KeyStart int    `yaml:"keyStart,omitempty"`
+	} `yaml:"ccOpt,omitempty"`
+	QueryArgs string `yaml:"queryArgs,omitempty"`
 }
