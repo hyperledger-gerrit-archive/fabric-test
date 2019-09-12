0d504a2b4f54d8ec821263ca69122e4abccc86b4
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-09-12 16:00:17 -0400

    [FAB-16585] Update conn profile after creating channel
    [FAB-16586] Update conn profile after joining peer
                to a channel
    
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: I6c6fea07e0d7fc3762c703b45d12be79497c61f7

--- tools/operator/connectionprofile/connectionprofile.go
+++ tools/operator/connectionprofile/connectionprofile.go
@@ -33,7 +33,7 @@ func (c ConnProfile) Organization(peerorg networkspec.PeerOrganizations, caList 
 	organization.AdminPrivateKey.Path = path
 	organization.SignedCert.Path = path
 	organization.CertificateAuthorities = append(organization.CertificateAuthorities, caList...)
-	for peer := range c.Peers{
+	for peer := range c.Peers {
 		peerList = append(peerList, peer)
 	}
 	organization.Peers = append(organization.Peers, peerList...)
@@ -43,28 +43,14 @@ func (c ConnProfile) Organization(peerorg networkspec.PeerOrganizations, caList 
 func (c ConnProfile) GenerateConnProfilePerOrg(orgName string) error {
 
 	var err error
-	var channel networkspec.Channel
-	var peersList, orderersList []string
 	path := paths.ConnectionProfilesDir(c.Config.ArtifactsLocation)
 	fileName := paths.JoinPath(path, fmt.Sprintf("connection_profile_%s.yaml", orgName))
-	channels := make(map[string]networkspec.Channel)
-	for i := 0; i < c.Config.NumChannels; i++ {
-		for orderer := range c.Orderers{
-			orderersList = append(orderersList, orderer)
-		}
-		for peer := range c.Peers{
-			peersList = append(peersList, peer)
-		}
-		channel = networkspec.Channel{Orderers: orderersList, Peers: peersList}
-		channelName := fmt.Sprintf("testorgschannel%d", i)
-		channels[channelName] = channel
-	}
 	client := networkspec.Client{Organization: orgName}
 	client.Conenction.Timeout.Peer.Endorser = 300
 	client.Conenction.Timeout.Peer.EventHub = 600
 	client.Conenction.Timeout.Peer.EventReg = 300
 	client.Conenction.Timeout.Orderer = 300
-	cp := networkspec.ConnectionProfile{Client: client, Channels: channels, Organizations: c.Organizations, Orderers: c.Orderers, Peers: c.Peers, CA: c.CA}
+	cp := networkspec.ConnectionProfile{Client: client, Organizations: c.Organizations, Orderers: c.Orderers, Peers: c.Peers, CA: c.CA}
 	yamlBytes, err := yaml.Marshal(cp)
 	if err != nil {
 		logger.ERROR("Failed to convert the connection profile struct to bytes")
@@ -83,4 +69,55 @@ func (c ConnProfile) GenerateConnProfilePerOrg(orgName string) error {
 	}
 	logger.INFO("Successfully created ", fileName)
 	return nil
+}
+
+func (c ConnProfile) UpdateConnectionProfile(connProfileFilePath, channelName, componentType string) error {
+
+	componentsList, connProfileObject, err := c.getComponentsListFromConnProfile(connProfileFilePath, componentType)
+	if err != nil {
+		logger.ERROR("Failed to get the components list from the connection profile file")
+		return err
+	}
+	switch componentType {
+	case "orderer":
+		connProfileObject.Channels[channelName] = networkspec.Channel{Orderers: componentsList}
+	case "peer":
+		channelObject := connProfileObject.Channels[channelName]
+		connProfileObject.Channels[channelName] = networkspec.Channel{Orderers: channelObject.Orderers, Peers: componentsList}
+	}
+	yamlBytes, err := yaml.Marshal(connProfileObject)
+	yamlBytes = append([]byte("version: 1.0 \nname: My network \ndescription: Connection Profile for Blockchain Network \n"), yamlBytes...)
+	err = ioutil.WriteFile(connProfileFilePath, yamlBytes, 0644)
+	if err != nil {
+		logger.ERROR("Failed to update connection profile")
+		return err
+	}
+	return err
+}
+
+func (c ConnProfile) getComponentsListFromConnProfile(connProfileFilePath, componentType string) ([]string, networkspec.ConnectionProfile, error) {
+
+	var componentsList []string
+	var err error
+	var connectionProfileObject networkspec.ConnectionProfile
+	yamlFile, err := ioutil.ReadFile(connProfileFilePath)
+	if err != nil {
+		logger.ERROR("Failed to read connection profile")
+		return componentsList, connectionProfileObject, err
+	}
+	err = yaml.Unmarshal(yamlFile, &connectionProfileObject)
+	if err != nil {
+		logger.ERROR("Failed to unmarshall yaml file")
+		return componentsList, connectionProfileObject, err
+	}
+	if componentType == "orderer" {
+		for key, _ := range connectionProfileObject.Orderers {
+			componentsList = append(componentsList, key)
+		}
+		return componentsList, connectionProfileObject, nil
+	}
+	for key, _ := range connectionProfileObject.Peers {
+		componentsList = append(componentsList, key)
+	}
+	return componentsList, connectionProfileObject, nil
 }
