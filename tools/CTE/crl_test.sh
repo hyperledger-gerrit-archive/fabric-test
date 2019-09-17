24d724992439290d0c3971ac1b5069a6457947dd
bharadwajambati1 <bharadwaj.ambati1@ibm.com>
2019-09-17 15:23:55 -0400

    [FAB-16627] Removing GOPATH from fabric-test/tools/CTE
    Signed-off-by: bharadwajambati1 <bharadwaj.ambati1@ibm.com>
    Change-Id: Id8f4acaa919fa1cbc83d60e5a7b424862c91081b

--- tools/CTE/crl_test.sh
+++ tools/CTE/crl_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 : ${TESTCASE:=gencrl}
-FABRIC_CA="$GOPATH/src/github.com/hyperledger/fabric-ca"
+CurrentDirectory=$(cd `dirname $0` && pwd)
+FabricTestDir=$CurrentDirectory/../../
+FABRIC_CA=$FabricTestDir"../fabric-ca"
 SCRIPTDIR="$FABRIC_CA/scripts/fvt"
 CA_CFG_PATH="/tmp/$TESTCASE"
 CONFIGFILE="$CA_CFG_PATH/runFabricCaFvt.yaml"
