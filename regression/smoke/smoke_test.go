39f6c308850c787ce32e599af917692f7719afec
Surya Lanka <suryalnvs@gmail.com>
2019-10-18 15:28:12 -0400

    [WIP - FAB-16529] Smoke test suite
    WIP - Smoke test suite using operator and testclient
    
    Signed-off-by: Surya Lanka <suryalnvs@gmail.com>
    Change-Id: I4891ff27649e0586eb54f0af589042d2f61d3871

---
+++ regression/smoke/smoke_test.go
@@ -0,0 +1,54 @@
+package smoke_test
+
+import (
+	//"time"
+
+	. "github.com/onsi/ginkgo"
+//	. "github.com/onsi/gomega"
+	
+	"github.com/hyperledger/fabric-test/tools/operator/testclient"
+)
+
+var _ = Describe("Smoke Test", func() {
+
+	Describe("Create channel and Joining peers to channel", func() {
+		var (
+			action string
+			inputSpecPath string
+		)
+
+		BeforeEach(func(){
+			inputSpecPath = "./pte-input.yml"
+            
+		})
+		
+		//AfterEach()
+        It("Running end to end", func() {
+			inputSpecPath = "./pte-input.yml"
+			
+			By("1) Creating channel")
+			action = "create"
+			testclient.Testclient(action, inputSpecPath)
+
+			By("2) Joining Peers to channel")
+			action = "join"
+			testclient.Testclient(action, inputSpecPath)
+
+			By("3) Updating channel with anchor peers")
+			action = "anchorpeer"
+			testclient.Testclient(action, inputSpecPath)
+
+			By("4) Installing Chaincode on Peers")
+			action = "install"
+			testclient.Testclient(action, inputSpecPath)
+
+			By("5) Instantiating Chaincode")
+			action = "instantiate"
+			testclient.Testclient(action, inputSpecPath)
+
+			By("6) Upgrading Chaincode")
+			action = "upgrade"
+			testclient.Testclient(action, inputSpecPath)
+        })
+	})
+})
