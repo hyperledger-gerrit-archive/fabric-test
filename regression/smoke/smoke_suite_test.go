07aa74d938feeff7db096f42155966fa065133a9
Surya Lanka <suryalnvs@gmail.com>
2019-10-18 15:28:12 -0400

    [WIP - FAB-16529] Smoke test suite
    WIP - Smoke test suite using operator and testclient
    
    Signed-off-by: Surya Lanka <suryalnvs@gmail.com>
    Change-Id: I4891ff27649e0586eb54f0af589042d2f61d3871

---
+++ regression/smoke/smoke_suite_test.go
@@ -0,0 +1,29 @@
+package smoke_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/ginkgo/reporters"
+
+	"github.com/hyperledger/fabric-test/tools/operator/launcher"
+)
+
+func TestSmoke(t *testing.T) {
+	RegisterFailHandler(Fail)
+	junitReporter := reporters.NewJUnitReporter("junit.xml")
+    RunSpecsWithDefaultAndCustomReporters(t, "Smoke Suite", []Reporter{junitReporter})
+}
+
+var _ = BeforeSuite(func() {
+	networkSpecPath := "./networkspec.yaml"
+	err := launcher.Launcher("up", "docker", "", networkSpecPath)
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func(){
+	networkSpecPath := "./networkspec.yaml"
+	err := launcher.Launcher("down", "docker", "", networkSpecPath)
+	Expect(err).NotTo(HaveOccurred())
+})
