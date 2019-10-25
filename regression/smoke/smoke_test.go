6f481abbbbf1b798d371b3ce1b9741eb9a4c4f35
Surya Lanka <suryalnvs@gmail.com>
2019-10-25 12:42:30 -0400

    [FAB-16529] - Smoke test suite with ginkgo
    Adding smoke test suite in ginkgo using operator tooling
    It can be run using command `ginkgo -v` from smoke directory
    
    It runs an end-to-end scenario which includes network setup,
    channel creation, joining peers, installing chaincode,
    instantiating chaincode, sending queries, sending invokes,
    upgrading chaincode, sending queries, deleting network and
    chaincode containers locally using networkspec.yml and
    pte-input.yml
    
    Signed-off-by: Surya Lanka <suryalnvs@gmail.com>
    Change-Id: I258a763958fb7fbf7b1b1bac082a1784503f4fb7

--- regression/smoke/smoke_test.go
+++ regression/smoke/smoke_test.go
@@ -1,67 +1,69 @@
 package smoke_test
 
 import (
-	//"time"
-
 	. "github.com/onsi/ginkgo"
-//	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 	
 	"github.com/hyperledger/fabric-test/tools/operator/testclient"
 )
 
 var _ = Describe("Smoke Test", func() {
 
-	Describe("Create channel and Joining peers to channel", func() {
+	Describe("Running Smoke Test Suite - fabric test", func() {
 		var (
 			action string
 			inputSpecPath string
 		)
 
-		BeforeEach(func(){
-			inputSpecPath = "./pte-input.yml"
-            
-		})
-		
-		//AfterEach()
         It("Running end to end", func() {
 			inputSpecPath = "./pte-input.yml"
 			
 			By("1) Creating channel")
 			action = "create"
-			testclient.Testclient(action, inputSpecPath)
+			err := testclient.Testclient(action, inputSpecPath)
+			Expect(err).NotTo(HaveOccurred())
 
 			By("2) Joining Peers to channel")
 			action = "join"
-			testclient.Testclient(action, inputSpecPath)
+			err = testclient.Testclient(action, inputSpecPath)
+			Expect(err).NotTo(HaveOccurred())
+
 
 			By("3) Updating channel with anchor peers")
 			action = "anchorpeer"
-			testclient.Testclient(action, inputSpecPath)
+			err = testclient.Testclient(action, inputSpecPath)
+			Expect(err).NotTo(HaveOccurred())
+
 
 			By("4) Installing Chaincode on Peers")
 			action = "install"
-			testclient.Testclient(action, inputSpecPath)
+			err = testclient.Testclient(action, inputSpecPath)
+			Expect(err).NotTo(HaveOccurred())
 
 			By("5) Instantiating Chaincode")
 			action = "instantiate"
-			testclient.Testclient(action, inputSpecPath)
+			err = testclient.Testclient(action, inputSpecPath)
+			Expect(err).NotTo(HaveOccurred())
 
 			By("6) Sending Queries")
 			action = "query"
-			testclient.Testclient(action, inputSpecPath)
+			err = testclient.Testclient(action, inputSpecPath)
+			Expect(err).NotTo(HaveOccurred())
 
 			By("7) Sending Invokes")
 			action = "invoke"
-			testclient.Testclient(action, inputSpecPath)
+			err = testclient.Testclient(action, inputSpecPath)
+			Expect(err).NotTo(HaveOccurred())
 
 			By("8) Upgrading Chaincode")
 			action = "upgrade"
-			testclient.Testclient(action, inputSpecPath)
+			err = testclient.Testclient(action, inputSpecPath)
+			Expect(err).NotTo(HaveOccurred())
 
 			By("9) Sending Queries")
 			action = "query"
 			testclient.Testclient(action, inputSpecPath)
-
+			Expect(err).NotTo(HaveOccurred())
         })
 	})
 })
